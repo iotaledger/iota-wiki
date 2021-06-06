ad12f1b586ddf908dab44b1cb391d09955d3c96b
Dr-Electron <dr-electr0n@protonmail.com>
2021-06-06 15:19:56 +0200

    Add first example of socials row

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -16,6 +16,60 @@ import useKeyboardNavigation from '@theme/hooks/useKeyboardNavigation';
 import {ThemeClassNames} from '@docusaurus/theme-common';
 import './styles.css';
 
+const FeatureList = [
+  {
+    title: 'Youtube',
+    imageUrl: '../../../static/img/youtube.svg',
+    url: 'https://www.youtube.com/c/iotafoundation',
+  },
+  {
+    title: 'Github',
+    imageUrl: '../../../static/img/github.svg',
+    url: 'https://www.github.com/iotaledger/',
+  },
+  {
+    title: 'Discord',
+    imageUrl: '../../../static/img/discord.svg',
+    url: 'https://discord.iota.org/',
+  },
+  {
+    title: 'Twitter',
+    imageUrl: '../../../static/img/twitter.svg',
+    url: 'https://www.twitter.com/iota/',
+  },
+  {
+    title: 'Reddit',
+    imageUrl: '../../../static/img/reddit.svg',
+    url: 'https://www.reddit.com/r/iota/',
+  },
+  {
+    title: 'LinkedIn',
+    imageUrl: '../../../static/img/linkedin.svg',
+    url: 'https://www.linkedin.com/company/iotafoundation/',
+  },
+  {
+    title: 'Instagram',
+    imageUrl: '../../../static/img/instagram.svg',
+    url: 'https://www.instagram.com/iotafoundation/',
+  },
+];
+
+function Feature({id, title, imageUrl, url }) {
+  //TO-DO Calculate color from inner col with id
+  return (
+    <div className='col'>
+      <a href={url}>
+      <div className='row'>
+          <div className='col'>
+            <img className='social' src={imageUrl} />
+          </div>
+          <div className='social col'>{title}</div>
+      </div>
+      </a>
+    </div>
+  );
+}
+
 function Layout(props) {
   const {children, noFooter, wrapperClassName, pageClassName} = props;
   useKeyboardNavigation();
@@ -40,6 +94,11 @@ function Layout(props) {
         </div>
       </div>
       {!noFooter && <Footer />}
+      <div className="row">
+        {FeatureList.map((props, idx) => (
+          <Feature key={idx} id={idx} {...props} />
+        ))}
+      </div>
     </LayoutProviders>
   );
 }
