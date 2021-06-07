7e3ba7c31d5d880b2df2ff7a9b3ce6f65a0b7b9c
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-06-07 23:28:54 +0200

    Add background colors

--- src/theme/SocialBar/index.js
+++ src/theme/SocialBar/index.js
@@ -8,43 +8,49 @@ const SocialLinkList = [
     title: 'Youtube',
     imageUrl: '/img/youtube.svg',
     url: 'https://www.youtube.com/c/iotafoundation',
+    backgroundColor: '#131F37',
   },
   {
     title: 'GitHub',
     imageUrl: '/img/github.svg',
     url: 'https://www.github.com/iotaledger/',
+    backgroundColor: '#3C4964',
   },
   {
     title: 'Discord',
     imageUrl: '/img/discord.svg',
     url: 'https://discord.iota.org/',
+    backgroundColor: '#69768F',
   },
   {
     title: 'Twitter',
     imageUrl: '/img/twitter.svg',
     url: 'https://www.twitter.com/iota/',
+    backgroundColor: '#8493AD',
   },
   {
     title: 'Reddit',
     imageUrl: '/img/reddit.svg',
     url: 'https://www.reddit.com/r/iota/',
+    backgroundColor: '#9FAFCA',
   },
   {
     title: 'Linkedin',
     imageUrl: '/img/linkedin.svg',
     url: 'https://www.linkedin.com/company/iotafoundation/',
+    backgroundColor: '#C3D0E4',
   },
   {
     title: 'Instagram',
     imageUrl: '/img/instagram.svg',
     url: 'https://www.instagram.com/iotafoundation/',
+    backgroundColor: '#D1DDEF',
   },
 ]
 
-function SocialLink({ id, title, imageUrl, url }) {
-  //TO-DO Calculate color from inner col with id
+function SocialLink({ title, imageUrl, url, backgroundColor }) {
   return (
-    <div className={clsx('col', styles.socialLink)}>
+    <div className={clsx('col', styles.socialLink)} style={{backgroundColor: backgroundColor}}>
       <a href={url}>
         <img src={useBaseUrl(imageUrl)} />
         <div>{title}</div>
@@ -57,7 +63,7 @@ function SocialBar() {
   return (
     <div className="row row--no-gutters text--center">
       {SocialLinkList.map((props, idx) => (
-        <SocialLink key={idx} id={idx} {...props} />
+        <SocialLink key={idx} {...props} />
       ))}
     </div>
   )
