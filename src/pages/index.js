231ccbaaf8ff63ee244c1d88a016b87ad3ecf2b3
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-06-15 00:12:03 +0200

    Add under construction page and prevent indexing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import LandingpageHeader from '../components/LandingpageHeader';
+import Link from '@docusaurus/Link';
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`${siteConfig.title}`} description={`${siteConfig.tagline}`}>
-      <LandingpageHeader />
+    <Layout title={`${siteConfig.title} WORK IN PROGRESS`} description={`${siteConfig.tagline}`}>
+      <div className='row margin-horiz--lg margin-vert--xl padding-vert--xl'>
+        <span
+          className="col col--2 col--offset-2 material-icons text--center"
+          style={{
+            fontSize: '10rem',
+          }}
+        >
+          construction
+        </span>
+        <div className='col col--4 padding-vert--md'>
+          <h1>Work in Progress</h1>
+          <p>
+            This website is under construction. Use the links in the header and footer to
+            get around or <Link to='/main'>continue to the main page</Link>.
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 }
