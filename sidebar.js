00c0ef04d4749fc2d6d3058832f0b9eeaeebe552
Adam Croply <61834907+AdamCroply@users.noreply.github.com>
2021-06-20 15:00:49 +0200

    Uploaded new sidebar.js file.
    Built a new sidebar.js with all the four new menus and all uploaded pages. There is probably some errors (and I don't know how to implement it within the design) so it will need someone to go through it, and then hook it up to the docosaurus system, and nav bar menu items.

---
+++ sidebar.js
@@ -0,0 +1,254 @@
+
+module.exports = 
+{   
+    docs: [
+    
+    {
+        'Learn',
+        {
+          type: 'category',
+          label: 'About IOTA',
+          collapsed: false,
+          items: [
+                '/docs/learn/about-iota/an-introduction-to-iota', 
+                '/docs/learn/about-iota/data-transfer', 
+                '/docs/learn/about-iota/value-transfer',
+                '/docs/learn/about-iota/why-is-iota-feeless',
+                '/docs/learn/about-iota/roadmap-to-decentralization',
+                '/docs/learn/about-iota/security',
+                '/docs/learn/about-iota/energy-efficiency',
+            ],
+        },
+        {
+          type: 'category',
+          label: 'IOTA Token',
+          collapsed: true,
+          items: [
+                '/docs/learn/iota-token/overview',
+                '/docs/learn/iota-token/buying-iota',
+                '/docs/learn/iota-token/exchanges',
+                '/docs/learn/iota-token/transfering-tokens',
+                '/docs/learn/iota-token/token-security',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Wallets',
+            collapsed: true,
+            items: [
+                  '/docs/learn/wallets/what-is-a-wallet',
+                  '/docs/learn/wallets/firefly-wallet',
+                  '/docs/learn/wallets/iota-devnet-wallet',
+                  '/docs/learn/wallets/hardware-wallets',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Networks',
+            collapsed: true,
+            items: [
+                  '/docs/learn/networks/iota-1.5',
+                  '/docs/learn/networks/iota-2.0',
+                  '/docs/learn/networks/devnets',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Resource Materials',
+            collapsed: true,
+            items: [
+                  '/docs/learn/resource-materials/glossary',
+                  '/docs/learn/resource-materials/fact-base',
+                  '/docs/learn/resource-materials/infographics',
+                  '/docs/learn/resource-materials/videos',
+                  '/docs/learn/resource-materials/faqs',
+            ],
+          },
+          {
+            type: 'category',
+            label: 'Research',
+            collapsed: true,
+            items: [
+                  '/docs/learn/research/research-outline',
+                  '/docs/learn/research/papers',
+                  '/docs/learn/research/grants',
+          ],
+          },
+
+    
+        'Use',
+        { 
+            type: 'category',
+            label: 'Use Cases',
+            collapsed: true,
+            items: [
+                  '/docs/use/use cases/industry-applications',
+                  '/docs/use/use cases/internet-of-things',
+                  '/docs/use/use cases/digital-assets',
+                  '/docs/use/use cases/nfts',
+                  '/docs/use/use cases/data-confidence',
+                  '/docs/use/use cases/eco-social-projects',
+                  '/docs/use/use cases/private-tangle',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Streams',
+            collapsed: true,
+            items: [
+                  '/docs/use/streams/encrypted data comms',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Identity',
+            collapsed: true,
+            items: [
+                  '/docs/use/identity/enabling-privacy-and-trust',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Smart Contracts',
+            collapsed: true,
+            items: [
+                  '/docs/use/smart-contracts/programmable-contracts',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Access',
+            collapsed: true,
+            items: [
+                  '/docs/use/access/secure-access-control',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Stronghold',
+            collapsed: true,
+            items: [
+                  '/docs/use/stronghold/protecting-your-secrets',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Oracles',
+            collapsed: true,
+            items: [
+                  '/docs/use/oracles/trust-in-real-world-data',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Utilities',
+            collapsed: true,
+            items: [
+                  '/docs/use/utilities/tangle-explorer',
+                  '/docs/use/utilities/visualizer',
+                  '/docs/use/utilities/iota-ipfs',
+            ],
+        }, 
+
+
+     'Participate',
+        {
+            type: 'category',
+            label: 'Support The Network',
+            collapsed: true,
+            items: [
+                  '/docs/participate/support-the-network/run-a-node',
+                  '/docs/participate/support-the-network/create-an-application',
+            ],
+        },        
+        {
+            type: 'category',
+            label: 'The Community',
+            collapsed: true,
+            items: [
+                  '/docs/participate/the-community/discord',
+                  '/docs/participate/the-community/x-teams',
+                  '/docs/participate/the-community/git-hub',
+                  '/docs/participate/the-community/stack-overflow',
+                  '/docs/participate/the-community/social-media',
+            ],
+        },        
+        {
+            type: 'category',
+            label: 'Partnerships',
+            collapsed: true,
+            items: [
+                  '/docs/participate/partnerships/iota-partnerships',
+            ],
+        },        
+        {
+            type: 'category',
+            label: 'Funding',
+            collapsed: true,
+            items: [
+                  '/docs/participate/funding/edf-funding',
+            ],
+        }, 
+    
+
+
+      'Develop',
+        {
+            type: 'category',
+            label: 'Getting Started',
+            collapsed: true,
+            items: [
+                  '/docs/develop/getting started/archtecture',
+                  '/docs/develop/getting started/environment',
+                  '/docs/develop/getting started/send-a-first-message',
+                  '/docs/develop/getting started/devnet',
+            ],
+        },        
+        {
+            type: 'category',
+            label: 'Fundamentals',
+            collapsed: true,
+            items: [
+                  '/docs/develop/fundamentals/cryptography',
+                  '/docs/develop/fundamentals/consensus',
+                  '/docs/develop/fundamentals/mana',
+                  '/docs/develop/fundamentals/utxo',
+                  '/docs/develop/fundamentals/dapps',
+            ],
+        },        
+        {
+            type: 'category',
+            label: 'Exchange Integration',
+            collapsed: true,
+            items: [
+                  '/docs/develop/exchange-integration/guide',
+            ],
+        },        
+        {
+            type: 'category',
+            label: 'Tutorials',
+            collapsed: true,
+            items: [
+                  '/docs/develop/tutorials/hackster',
+                  '/docs/develop/tutorials/youtube',
+            ],
+        },        
+        {
+            type: 'category',
+            label: 'Developer Docs',
+            collapsed: true,
+            items: [
+                  '/docs/develop/getting started/archtecture',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Project Repos',
+            collapsed: true,
+            items: [
+                  '/docs/develop/getting started/archtecture',
+            ],
+        },
+
+    ],   
+};
