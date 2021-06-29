9264ac062a58d35b6196cf77f9da878b8b179a96
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-06-29 22:57:47 +0200

    Group similar items

--- src/components/EditorMenu/index.js
+++ src/components/EditorMenu/index.js
@@ -40,90 +40,85 @@ export default function EditorMenu({ editor, className }) {
   }
 
   return (
-    <div className={clsx(className, styles.menu)}>
-      <span
-        onClick={() => editor.chain().focus().undo().run()}
-        className='material-icons'
-      >
-        undo
-      </span>
-      <span
-        onClick={() => editor.chain().focus().redo().run()}
-        className='material-icons'
-      >
-        redo
-      </span>
-      <select
-        value={checkFontStyle()}
-        onChange={changeFontStyle}>
-        <option hidden disabled selected value=''></option>
-        <option value="paragraph">Normal text</option>
-        {
-          headingLevels.map((level) => {
-            return <option key={level} value={level.toString()}>{`Heading ${level}`}</option>
-          })
-        }
-      </select>
-      <span
-        onClick={() => editor.chain().focus().setHorizontalRule().run()}
-        className='material-icons'
-      >
-        horizontal_rule
-      </span>
-      <span
-        onClick={() => editor.chain().focus().setHardBreak().run()}
-        className='material-icons'
-      >
-        keyboard_return
-      </span>
-      <span
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        className={clsx('material-icons', editor.isActive('bold') ? 'is-active' : '')}
-      >
-        format_bold
-      </span>
-      <span
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        className={clsx('material-icons', editor.isActive('italic') ? 'is-active' : '')}
-      >
-        format_italic
-      </span>
-      <span
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        className={clsx('material-icons', editor.isActive('code') ? 'is-active' : '')}
-      >
-        code
-      </span>
-      <span
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={clsx('material-icons', editor.isActive('bulletList') ? 'is-active' : '')}
-      >
-        format_list_bulleted
-      </span>
-      <span
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={clsx('material-icons', editor.isActive('orderedList') ? 'is-active' : '')}
-      >
-        format_list_numbered
-      </span>
-      <span
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={clsx('material-icons', editor.isActive('codeBlock') ? 'is-active' : '')}
-      >
-        code
-      </span>
-      <span
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={clsx('material-icons', editor.isActive('blockquote') ? 'is-active' : '')}
-      >
-        format_quote
-      </span>
-      <span
-        onClick={() => editor.chain().focus().unsetAllMarks().clearNodes().run()}
-        className='material-icons'
-      >
-        format_clear
-      </span>
+    <div className={clsx(className, styles.menu, 'material-icons')}>
+      <div className={styles.history}>
+        <span onClick={() => editor.chain().focus().undo().run()}>undo</span>
+        <span onClick={() => editor.chain().focus().redo().run()}>redo</span>
+      </div>
+      <div className={styles.style}>
+        <select value={checkFontStyle()} onChange={changeFontStyle}>
+          <option hidden disabled value=''></option>
+          <option value="paragraph">Normal text</option>
+          {headingLevels.map((level) => {
+            return <option key={level} value={level}>{`Heading ${level}`}</option>
+          })}
+        </select>
+      </div>
+      <div className={styles.format}>
+        <span
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={clsx(editor.isActive('bold') ? 'is-active' : '')}
+        >
+          format_bold
+        </span>
+        <span
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={clsx(editor.isActive('italic') ? 'is-active' : '')}
+        >
+          format_italic
+        </span>
+        <span
+          onClick={() => editor.chain().focus().toggleCode().run()}
+          className={clsx(editor.isActive('code') ? 'is-active' : '')}
+        >
+          code
+        </span>
+      </div>
+      <div className={styles.layout}>
+        <span
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={clsx(editor.isActive('bulletList') ? 'is-active' : '')}
+        >
+          format_list_bulleted
+        </span>
+        <span
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={clsx(editor.isActive('orderedList') ? 'is-active' : '')}
+        >
+          format_list_numbered
+        </span>
+        <span
+          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          className={clsx(editor.isActive('codeBlock') ? 'is-active' : '')}
+        >
+          code
+        </span>
+        <span
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={clsx(editor.isActive('blockquote') ? 'is-active' : '')}
+        >
+          format_quote
+        </span>
+      </div>
+      <div className={styles.elements}>
+        <span
+          onClick={() => editor.chain().focus().setHorizontalRule().run()}
+        >
+          horizontal_rule
+        </span>
+        <span
+          onClick={() => editor.chain().focus().setHardBreak().run()}
+        >
+          keyboard_return
+        </span>
+      </div>
+      <div className={styles.actions}>
+        <span
+          onClick={() => editor.chain().focus().unsetAllMarks().clearNodes().run()}
+        >
+          format_clear
+        </span>
+      </div>
     </div>
   )
 }
