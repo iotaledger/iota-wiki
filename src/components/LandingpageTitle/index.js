7a92556c1c1a37adf862144483d710b2aad607a3
Dr-Electron <61637085+Dr-Electron@users.noreply.github.com>
2021-07-30 01:09:18 +0200

    Fix images for light and dark theme (#82)

--- src/components/LandingpageTitle/index.js
+++ src/components/LandingpageTitle/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import useThemeContext from '@theme/hooks/useThemeContext';
 import useBaseUrl from '@docusaurus/useBaseUrl'
 import styles from './styles.module.css';
 
 
 function LandingpageTitle() {
   const { siteConfig } = useDocusaurusContext();
+  const { isDarkTheme } = useThemeContext();
 
   return (
     <div className={clsx(styles.heading)}>
-      <img className={clsx(styles.headingImage)} src={useBaseUrl('/img/globe.svg')} />
+      <img className={clsx(styles.headingImage)} src={isDarkTheme ? useBaseUrl('/img/globe_dark.svg') : useBaseUrl('/img/globe_light.svg')} />
       <div>
         <h1 className={clsx(styles.headingTitle)}>{siteConfig.title}</h1>
         <span className={clsx('grey', 'section-header')}>{siteConfig.tagline}</span>
