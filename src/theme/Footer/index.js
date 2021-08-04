bb1370ce70d635804fafd6c950ff1a711fb0600f
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-08-04 13:24:04 +0200

    Restyle landing page (#99)
    * Merge components into one
    
    * Use BEM naming
    
    * Fix imports
    
    * adjust spacing

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -55,65 +55,63 @@ function Footer() {
         className={clsx('footer', 'padding--none', 'padding-top--lg', {
           'footer--dark': footer.style === 'dark',
         })}>
-        <div className="container padding-horiz--xl">
-          {links && links.length > 0 && (
-            <div className="row footer__links padding-top--lg padding-bottom--xl">
-              {links.map((linkItem, i) => (
-                <div key={i} className="col footer__col">
-                  {linkItem.title != null ? (
-                    <h4 className="footer__title">{linkItem.title}</h4>
-                  ) : null}
-                  {linkItem.items != null &&
-                  Array.isArray(linkItem.items) &&
-                  linkItem.items.length > 0 ? (
-                    <ul className="footer__items">
-                      {linkItem.items.map((item, key) =>
-                        item.html ? (
-                          <li
-                            key={key}
-                            className="footer__item" // Developer provided the HTML, so assume it's safe.
-                            // eslint-disable-next-line react/no-danger
-                            dangerouslySetInnerHTML={{
-                              __html: item.html,
-                            }}
-                          />
-                        ) : (
-                          <li key={item.href || item.to} className="footer__item">
-                            <FooterLink {...item} />
-                          </li>
-                        ),
-                      )}
-                    </ul>
-                  ) : null}
-                </div>
-              ))}
-            </div>
-          )}
-          {(logo || copyright) && (
-            <div className="footer__bottom padding-top--xl padding-bottom--lg">
-              {logo && (logo.src || logo.srcDark) && (
-                <div>
-                  {logo.href ? (
-                    <Link href={logo.href}>
-                      <FooterLogo alt={logo.alt} sources={sources} />
-                    </Link>
-                  ) : (
+        {links && links.length > 0 && (
+          <div className="row footer__links padding-top--lg padding-bottom--xl">
+            {links.map((linkItem, i) => (
+              <div key={i} className="col footer__col">
+                {linkItem.title != null ? (
+                  <h4 className="footer__title">{linkItem.title}</h4>
+                ) : null}
+                {linkItem.items != null &&
+                Array.isArray(linkItem.items) &&
+                linkItem.items.length > 0 ? (
+                  <ul className="footer__items">
+                    {linkItem.items.map((item, key) =>
+                      item.html ? (
+                        <li
+                          key={key}
+                          className="footer__item" // Developer provided the HTML, so assume it's safe.
+                          // eslint-disable-next-line react/no-danger
+                          dangerouslySetInnerHTML={{
+                            __html: item.html,
+                          }}
+                        />
+                      ) : (
+                        <li key={item.href || item.to} className="footer__item">
+                          <FooterLink {...item} />
+                        </li>
+                      ),
+                    )}
+                  </ul>
+                ) : null}
+              </div>
+            ))}
+          </div>
+        )}
+        {(logo || copyright) && (
+          <div className="footer__bottom padding-top--xl padding-bottom--lg">
+            {logo && (logo.src || logo.srcDark) && (
+              <div>
+                {logo.href ? (
+                  <Link href={logo.href}>
                     <FooterLogo alt={logo.alt} sources={sources} />
-                  )}
-                </div>
-              )}
-              {copyright ? (
-                <div
-                  className="footer__copyright" // Developer provided the HTML, so assume it's safe.
-                  // eslint-disable-next-line react/no-danger
-                  dangerouslySetInnerHTML={{
-                    __html: copyright,
-                  }}
-                />
-              ) : null}
-            </div>
-          )}
-        </div>
+                  </Link>
+                ) : (
+                  <FooterLogo alt={logo.alt} sources={sources} />
+                )}
+              </div>
+            )}
+            {copyright ? (
+              <div
+                className="footer__copyright" // Developer provided the HTML, so assume it's safe.
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: copyright,
+                }}
+              />
+            ) : null}
+          </div>
+        )}
         <SocialBar />
       </footer>
     </>
