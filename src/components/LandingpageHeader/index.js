dd671fa4faa18480307f4abb86f84fd58c849b6a
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-08-04 02:07:09 +0200

    adjust spacing

--- src/components/LandingpageHeader/index.js
+++ src/components/LandingpageHeader/index.js
@@ -49,29 +49,31 @@ function Action({ title, link, description }) {
   }
 
   return (
-    <div
-      className='action margin-vert--md padding--lg'
-      onClick={handleClick}
-      onMouseOver={() => setHovering(true)}
-      onMouseOut={() => setHovering(false)}
-    >
-      <div className='action__header'>
-        <span className='action__title'>{title}</span>
-        <div href={link} className='action__button'>
-          <span className='action__icon material-icons'>
-            navigate_next
-          </span>
+    <div className='col col--4 margin-vert--md'>
+      <div
+        className='action padding--lg'
+        onClick={handleClick}
+        onMouseOver={() => setHovering(true)}
+        onMouseOut={() => setHovering(false)}
+      >
+        <div className='action__header'>
+          <span className='action__title'>{title}</span>
+          <div href={link} className='action__button'>
+            <span className='action__icon material-icons'>
+              navigate_next
+            </span>
+          </div>
         </div>
-      </div>
-      <div className={clsx(
-        'headline-stick',
-        {
-          'size-m': hovering,
-          'size-s': !hovering
-        }
-      )}></div>
-      <div className='action__description'>
-        {description}
+        <div className={clsx(
+          'headline-stick',
+          {
+            'size-m': hovering,
+            'size-s': !hovering
+          }
+        )}></div>
+        <div className='action__description'>
+          {description}
+        </div>
       </div>
     </div>
   );
@@ -82,17 +84,17 @@ function LandingpageHeader() {
   const { isDarkTheme } = useThemeContext();
 
   return (
-    <header className='header padding-top--xl'>
-      <div className='title'>
+    <header className='header padding-vert--xl'>
+      <div className='title margin-horiz--sm'>
         <img className='title__image' src={isDarkTheme ? useBaseUrl('/img/globe_dark.svg') : useBaseUrl('/img/globe_light.svg')} />
         <div>
           <h1 className='title__text'>{siteConfig.title}</h1>
           <span className='title__subtext grey'>{siteConfig.tagline}</span>
         </div>
       </div>
-      <div className='padding--xl'>
+      <div className='margin-top--xl'>
         <div className='section-header grey text--center margin-bottom--sm' >Get started, right away</div>
-        <div className='actionlist'>
+        <div className='actionlist row'>
           {ActionList.map((props, idx) => (
             <Action key={idx} {...props} />
           ))}
