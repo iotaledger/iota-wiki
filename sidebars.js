17b2c38ed92ffbd74f3daf38f917ca491f9be6a7
Dr-Electron <dr-electr0n@protonmail.com>
2021-08-20 17:50:51 +0200

    Update sidebar.js

--- sidebars.js
+++ sidebars.js
@@ -69,7 +69,7 @@ module.exports =
             collapsed: true,
             items: [
                 'learn/research/research-outline',
-                'learn/research/papers',
+                'learn/research/research-papers',
                 'learn/research/grants',
           ],
         },
