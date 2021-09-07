22a5a8561a5252e6f095bc26e4ef2ac7309b517e
lucastortora <lucas.tortora@iota.org>
2021-09-07 08:56:47 +0200

    Forcing local deploy on gh pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ module.exports = {
   title: 'IOTA Wiki',
   tagline: 'The complete reference for IOTA',
   url: 'https://wiki.iota.org',
-  baseUrl: '/',
+  baseUrl: '/iota-wiki/',
   onBrokenLinks: 'log',
   onBrokenMarkdownLinks: 'log',
   favicon: 'img/favicon.ico',
