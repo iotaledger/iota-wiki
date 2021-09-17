504eec4607406c15418818db13513fc04a37b069
Eike Haß <eike-hass@web.de>
2021-09-17 09:40:21 +0200

    fix pack for windows

--- cli/lib/commands/clean.js
+++ cli/lib/commands/clean.js
@@ -3,11 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const command_1 = require("@oclif/command");
 const path_1 = require("path");
 const fs_1 = require("fs");
+const local_config_1 = require("../local-config");
 class Clean extends command_1.Command {
     async run() {
         var _a;
         const PWD = (_a = process.env.PWD) !== null && _a !== void 0 ? _a : '';
-        const LOCAL_WIKI_FOLDER = path_1.join(PWD, 'local');
+        const userConfig = await local_config_1.getLocalConfig();
+        const LOCAL_WIKI_FOLDER = path_1.join(PWD, userConfig.localWikiFolder);
         fs_1.rmSync(LOCAL_WIKI_FOLDER, { recursive: true, force: true });
     }
 }
