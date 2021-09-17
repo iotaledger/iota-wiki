7127be46e722d7f36cc84055d5789d31f36dbfed
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-09-17 16:34:57 +0200

    Change landing page header to use Infima components

--- src/components/LandingpageHeader/index.js
+++ src/components/LandingpageHeader/index.js
@@ -1,74 +1,50 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { useHistory } from "react-router-dom";
-import clsx from 'clsx'
 
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import useThemeContext from '@theme/hooks/useThemeContext';
 
-
-import './styles.css'
-
-const ActionList = [
+const actions = [
   {
     title: 'Learn',
     image: 'img/learn.svg',
     link: 'docs/learn/about-iota/an-introduction-to-iota',
-    description: (
-      <>
-        Learn about IOTA, the Tangle, its features, industry applications, network and more.
-      </>
-    ),
+    description: 'Learn about IOTA, the Tangle, its features, industry applications, network and more.',
   },
   {
     title: 'Participate',
     image: 'img/participate.svg',
     link: 'docs/participate/support-the-network/about-nodes',
-    description: (
-      <>
-        Join the network and start using solutions built on top of the Tangle.
-      </>
-    ),
+    description: 'Join the network and start using solutions built on top of the Tangle.',
   },
   {
     title: 'Build',
     image: 'img/build.svg',
     link: 'docs/build/getting-started/architecture',
-    description: (
-      <>
-        Access documentation and guides to build with IOTA in Rust, C, Go, Java or Python.
-      </>
-    ),
+    description: 'Access documentation and guides to build with IOTA in Rust, C, Go, Java or Python.',
   },
 ];
 
-function Action({ title, image, image_hover, link, description }) {
-  let [hovering, setHovering] = useState(false);
+function Action({ title, image, link, description }) {
   let history = useHistory();
 
   const handleClick = (e) => {
     e.preventDefault();
-    history.push(link);
+    history.push(useBaseUrl(link));
   }
 
   return (
-    <div className='col col--4 margin-vert--md'>
+    <div className='col margin-vert--md'>
       <div
         className='card'
         onClick={handleClick}
-        onMouseOver={() => setHovering(true)}
-        onMouseOut={() => setHovering(false)}
       >
         <div className='card__image'>
-          <img className='image' src={image}/>
+          <img src={useBaseUrl(image)}/>
         </div>
-        <div className='card__body'>
+        <div className='card__body padding-horiz--lg padding-bottom--lg'>
           <h3>{title}</h3>
-          <div className={clsx(
-            'stick',
-            hovering ? 'stick--md' : 'stick--sm',
-          )}></div>
-          <div className='card__description'>{description}</div>
+          {description}
         </div>
       </div>
     </div>
@@ -77,24 +53,23 @@ function Action({ title, image, image_hover, link, description }) {
 
 function LandingpageHeader() {
   const { siteConfig } = useDocusaurusContext();
-  const { isDarkTheme } = useThemeContext();
 
   return (
-    <header className='header padding-vert--xl'>
-      <div className='title margin-horiz--sm'>
-        <div>
-          <h1 className='title__text'>{siteConfig.title}</h1>
-          <div className='title__subtext'>{siteConfig.tagline}</div>
+    <div className='hero'>
+      <div className='container'>
+        <div className='text--center margin-bottom--lg'>
+          <h1 className='hero__title margin--none'>{siteConfig.title}</h1>
+          <p className="hero__subtitle margin--none">{siteConfig.tagline}</p>
         </div>
+        {actions && (
+          <div className='row'>
+            {actions.map((props, idx) => (
+              <Action key={idx} {...props} />
+            ))}
+          </div>
+        )}
       </div>
-      <div className='card-container'>
-        <div className='card-container__row row'>
-          {ActionList.map((props, idx) => (
-            <Action key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </header>
+    </div>
   )
 }
 
