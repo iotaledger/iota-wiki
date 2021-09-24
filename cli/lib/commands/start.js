666681c81ff0614a825514f0c3aa1497c564d9a8
Dr-Electron <dr-electr0n@protonmail.com>
2021-09-24 20:58:24 +0200

    Pack stuff ;)

--- cli/lib/commands/start.js
+++ cli/lib/commands/start.js
@@ -11,27 +11,29 @@ const debounce = require('lodash.debounce');
 const local_config_1 = require("../local-config");
 class Start extends command_1.Command {
     async run() {
-        var _a;
+        var _a, _b, _c, _d;
         const PWD = (_a = process.env.PWD) !== null && _a !== void 0 ? _a : '';
         const userConfig = await local_config_1.getLocalConfig();
-        const WIKI_GIT_FOLDER = path_1.join(PWD, userConfig.localWikiFolder, 'iota-wiki');
-        const DOCUSAURUS_CONFIG_PATH = path_1.join(WIKI_GIT_FOLDER, 'docusaurus.config.js');
+        const WORKING_FOLDER = path_1.join(PWD, (_b = userConfig.localWikiFolder) !== null && _b !== void 0 ? _b : '', userConfig.localWikiFolder ? 'iota-wiki' : '');
+        const DOCUSAURUS_CONFIG_PATH = path_1.join(WORKING_FOLDER, 'docusaurus.config.js');
         const log = this.log;
-        const EXTERNAL_DOCS_CONFIG = fs_1.readFileSync(path_1.join(PWD, 'EXTERNAL_DOCS_CONFIG'), 'utf8');
+        const EXTERNAL_DOCS_CONFIG = fs_1.readFileSync(path_1.join(PWD, (_c = userConfig.configFolder) !== null && _c !== void 0 ? _c : '', 'EXTERNAL_DOCS_CONFIG'), 'utf8');
         await replaceInFile({
             files: DOCUSAURUS_CONFIG_PATH,
             from: /\/\* AUTO GENERATED EXTERNAL DOCS CONFIG \*\//,
             to: EXTERNAL_DOCS_CONFIG,
         });
-        const EXTERNAL_DOCS_DROPDOWN_CONFIG = fs_1.readFileSync(path_1.join(PWD, 'EXTERNAL_DOCS_DROPDOWN_CONFIG'), 'utf8');
+        const EXTERNAL_DOCS_DROPDOWN_CONFIG = fs_1.readFileSync(path_1.join(PWD, (_d = userConfig.configFolder) !== null && _d !== void 0 ? _d : '', 'EXTERNAL_DOCS_DROPDOWN_CONFIG'), 'utf8');
         await replaceInFile({
             files: DOCUSAURUS_CONFIG_PATH,
             from: /\/\* AUTO GENERATED EXTERNAL DOCS DROPDOWN CONFIG \*\//,
             to: EXTERNAL_DOCS_DROPDOWN_CONFIG,
         });
-        const WIKI_EXTERNAL_FOLDER = path_1.join(WIKI_GIT_FOLDER, 'external');
+        if (!userConfig.localWikiFolder)
+            return;
+        const WIKI_EXTERNAL_FOLDER = path_1.join(WORKING_FOLDER, 'external');
         const WIKI_CONTENT_REPO_FOLDER = path_1.join(WIKI_EXTERNAL_FOLDER, userConfig.repoName);
-        fs_extra_1.copySync(path_1.join(PWD, 'static', 'img'), path_1.join(WIKI_GIT_FOLDER, 'static', 'img'));
+        fs_extra_1.copySync(path_1.join(PWD, 'static', 'img'), path_1.join(WORKING_FOLDER, 'static', 'img'));
         log(path_1.resolve(path_1.join(PWD, '..')));
         const runYarn = debounce(() => {
             child_process_1.spawn('yarn', [
@@ -39,7 +41,7 @@ class Start extends command_1.Command {
                 '--host',
                 '0.0.0.0',
             ], {
-                cwd: WIKI_GIT_FOLDER,
+                cwd: WORKING_FOLDER,
                 shell: true,
                 stdio: 'inherit',
             });
