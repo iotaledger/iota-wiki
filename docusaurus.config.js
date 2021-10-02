56980cf73cb2e2adba1ebb2203c13042a1ea616f
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-10-02 22:57:09 +0200

    Move socials to the Docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -303,6 +303,56 @@ module.exports = {
       },
       copyright: `© ${new Date().getFullYear()} IOTA Wiki. Built with Docusaurus.`,
     },
+    socials: [
+      {
+        title: 'Youtube',
+        icon: '\ue907',
+        url: 'https://www.youtube.com/c/iotafoundation',
+        backgroundColor: 'var(--ifm-color-gray-900)',
+      },
+      {
+        title: 'GitHub',
+        icon: '\ue902',
+        url: 'https://www.github.com/iotaledger/',
+        backgroundColor: '#2C3850',
+      },
+      {
+        title: 'Discord',
+        icon: '\ue900',
+        url: 'https://discord.iota.org/',
+        backgroundColor: '#4B576F',
+      },
+      {
+        title: 'Twitter',
+        icon: '\ue906',
+        url: 'https://www.twitter.com/iota/',
+        backgroundColor: '#6A768E',
+      },
+      {
+        title: 'Reddit',
+        icon: '\ue905',
+        url: 'https://www.reddit.com/r/iota/',
+        backgroundColor: '#7D89A1',
+      },
+      {
+        title: 'Linkedin',
+        icon: '\ue904',
+        url: 'https://www.linkedin.com/company/iotafoundation/',
+        backgroundColor: '#8995AD',
+      },
+      {
+        title: 'Instagram',
+        icon: '\ue903',
+        url: 'https://www.instagram.com/iotafoundation/',
+        backgroundColor: '#99A5BD',
+      },
+      {
+        title: 'Facebook',
+        icon: '\ue901',
+        url: 'https://www.facebook.com/TheIOTAFoundation/',
+        backgroundColor: '#BAC6DE',
+      },
+    ],
     colorMode: {
       defaultMode: 'dark',
       // Dark/light switch icon options
