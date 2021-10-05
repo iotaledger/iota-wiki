fe51717ca872daf5956c7f499276d5a4f412089b
Dr-Electron <dr-electr0n@protonmail.com>
2021-10-06 00:26:19 +0200

    Check config

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -4,20 +4,19 @@ import TeamMember from './member';
 import config from './config.json';
 
 export default function Team() {
-    return (
-        <Layout>
-            <div className='hero'>
-                <div className='container'>
-                    <div className='text--center margin-bottom--lg'>
-                        <h1 className='hero__title margin--none'>Wiki Team</h1>
-                    </div>
-                    <div className='row'>
-                        {config.map((props, idx) => (
-                            <TeamMember key={idx} {...props} />
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div className='hero'>
+        <div className='container'>
+          <div className='text--center margin-bottom--lg'>
+            <h1 className='hero__title margin--none'>Wiki Team</h1>
+          </div>
+          <div className='row'>
+            {config &&
+              config.map((props, idx) => <TeamMember key={idx} {...props} />)}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
