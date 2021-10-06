497c88779a9cffb33eb5c1748ff97a1225557536
Critical94 <kilianhelene@web.de>
2021-10-06 02:48:44 +0200

    Add styling and fix links

--- src/theme/Card/index.js
+++ src/theme/Card/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import style from './styles.css';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export default function Card({ title, link, description, icon }) {
-    const handleClick = (e) => {
-        e.preventDefault();
-        history.push(link);
-    };
+  let history = useHistory();
+  const handleClick = (e) => {
+    e.preventDefault();
+    history.push(link);
+  };
 
-    return (
-        <div class="card card--pointer margin-horiz--md margin-vert--md shadow--md" onClick={handleClick}>
-            <div class="card__header">
-                <h3>{title}</h3>
-            </div>
-            <div class="card__body icon">
-                {icon}
-            </div>
-            <div class="card__footer">
-                <p>{description}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className='card card--pointer margin-horiz--md margin-vert--md shadow--md'
+      onClick={handleClick}
+    >
+      <div className='card__header'>
+        <h3>{title}</h3>
+      </div>
+      <div className='card__body icon'>{icon}</div>
+      <div className='card__footer'>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 }
+
+Card.propTypes = {
+  title: PropTypes.string,
+  icon: PropTypes.string,
+  link: PropTypes.string,
+  description: PropTypes.string,
+};
