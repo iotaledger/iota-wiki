4b25175742a06a5b34c71adcb1c2aee663f6de0e
Phyloiota <77154511+Phyloiota@users.noreply.github.com>
2021-10-24 14:51:43 +0800

    Create community-tutorials.md (#360)
    * Create community-tutorials.md
    
    Added the tutorials from ZAIRED
    
    * Update sidebars.js
    
    added community tutorials in the sidebar
    
    * Update community-tutorials.md

--- internal/learn/sidebars.js
+++ internal/learn/sidebars.js
@@ -78,6 +78,7 @@ module.exports = {
             // 'future/smart-contracts/smart-contracts-dapps',
             // 'future/smart-contracts/smart-contracts-consensus',
             // 'future/smart-contracts/smart-contracts-tangle',
+            'future/smart-contracts/smart-contracts-community-tutorials',
           ],
         },
       ],
