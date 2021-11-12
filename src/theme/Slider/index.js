8180cb36a2d31cf6b93c4daeef21ea1376a85858
Critical94 <kilianhelene@web.de>
2021-11-12 19:04:29 +0100

    Dynamic path

--- src/theme/Slider/index.js
+++ src/theme/Slider/index.js
@@ -3,22 +3,25 @@ import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import PropTypes from 'prop-types';
 
-export default function ImageSlider() {
+export default function ImageSlider({ path }) {
   // Import images from the infographics folder
   function importImages(r) {
     return r.keys().map((x) => x.replace('./', ''));
   }
+
   const allImages = importImages(
-    require.context('/img/infographics/', false, /\.(png|jpe?g|svg)$/),
+    require.context('/img/', true, /\.(png|jpe?g|svg)$/),
   );
+
+  const requestedImages = allImages.filter((word) => word.startsWith(path));
 
   // Create an array of objects with the image paths
   function createImageArray() {
     let images = [];
-    for (let i = 0; i < allImages.length; i++) {
+    for (let i = 0; i < requestedImages.length; i++) {
       images.push({
-        original: `/img/infographics/${allImages[i]}`,
-        thumbnail: `/img/infographics/${allImages[i]}`,
+        original: `/img/${requestedImages[i]}`,
+        thumbnail: `/img/${requestedImages[i]}`,
       });
     }
     return images;
@@ -31,4 +34,5 @@ export default function ImageSlider() {
 
 ImageSlider.propTypes = {
   images: PropTypes.array,
+  path: PropTypes.string,
 };
