ecf4f7bfca7557d5050e74d44ae0cd5d258f47e1
Critical94 <kilianhelene@web.de>
2021-11-16 22:19:58 +0100

    Add missing base Url

--- src/theme/Slider/index.js
+++ src/theme/Slider/index.js
@@ -29,7 +29,7 @@ export default function ImageSlider({ path }) {
       if (getFileExtension(requestedImages[i]) === 'mp4') {
         images.push({
           original: image,
-          thumbnail: '/img/infographics/video-placeholder.png',
+          thumbnail: useBaseUrl('/img/infographics/video-placeholder.png'),
           renderItem: () => (
             <video
               controls
