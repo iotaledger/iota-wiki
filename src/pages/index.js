f25f35055688bbcb89af348abf7709c656e8aeb3
Dr-Electron <dr-electr0n@protonmail.com>
2021-11-21 23:48:40 +0100

    Add HomeLayout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
-import CardWithImage from '/src/theme/CardWithImage';
-import Layout from '@theme/Layout';
+import HomeLayout from '/src/theme/HomeLayout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { useThemeConfig } from '@docusaurus/theme-common';
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
-  const { cards } = useThemeConfig();
 
   return (
-    <Layout description={`${siteConfig.tagline}`}>
-      <div className='hero'>
-        <div className='container'>
-          <div className='text--center margin-bottom--lg'>
-            <h1 className='hero__title margin--none'>{siteConfig.title}</h1>
-            <p className='hero__subtitle margin--none'>{siteConfig.tagline}</p>
-          </div>
-          {cards && (
-            <div className='row'>
-              {cards.map((props, idx) => (
-                <CardWithImage key={idx} {...props} />
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
-    </Layout>
+    <HomeLayout
+      description={siteConfig.tagline}
+      title={siteConfig.title}
+      tagline={siteConfig.tagline}
+    />
   );
 }
--- src/pages/legacy.js
+++ src/pages/legacy.js
@@ -1,33 +1,14 @@
 import React from 'react';
-import CardWithImage from '/src/theme/CardWithImage';
-import Layout from '@theme/Layout';
-import { useThemeConfig } from '@docusaurus/theme-common';
+import HomeLayout from '/src/theme/HomeLayout';
 
 export default function LegacyHome() {
-    const siteConfig = {
-        description: "The IOTA legacy documentation is no longer available.The IOTA Wiki is now the one source of truth for IOTA documentation.",
-        title: "Legacy Documentation",
-        tagline: "The IOTA legacy documentation is no longer available. Any documentation which is still valid will be ported to this Wiki, the one source of truth for all the IOTA documentation."
-    }
-    const { cards } = useThemeConfig();
+  const description =
+    'The IOTA legacy documentation is no longer available.The IOTA Wiki is now the one source of truth for IOTA documentation.';
+  const title = 'Legacy Documentation';
+  const tagline =
+    'The IOTA legacy documentation is no longer available. Any documentation which is still valid will be ported to this Wiki, the one source of truth for all the IOTA documentation.';
 
-    return (
-        <Layout description={`${siteConfig.description}`}>
-            <div className='hero'>
-                <div className='container'>
-                    <div className='text--center margin-bottom--lg'>
-                        <h1 className='hero__title margin--none'>{siteConfig.title}</h1>
-                        <p className='hero__subtitle margin--none'>{siteConfig.tagline}</p>
-                    </div>
-                    {cards && (
-                        <div className='row'>
-                            {cards.map((props, idx) => (
-                                <CardWithImage key={idx} {...props} />
-                            ))}
-                        </div>
-                    )}
-                </div>
-            </div>
-        </Layout>
-    );
+  return (
+    <HomeLayout description={description} title={title} tagline={tagline} />
+  );
 }
