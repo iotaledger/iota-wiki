6efae69c10786053b5bea6dcfed96cf3d448862b
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-11-28 23:50:14 +0100

    Add basic functionality

--- src/helper/fullscreenImages.js
+++ src/helper/fullscreenImages.js
@@ -1,2 +1,26 @@
-const matches = document.querySelectorAll('.markdown img');
-console.log(matches);
+const container = document.createElement('div');
+container.classList.add('fullscreen-image-container');
+
+const image = document.createElement('img');
+image.classList.add('fullscreen-image-container__image');
+
+container.appendChild(image);
+
+function openImage(event) {
+  container.classList.add('fullscreen-image-container--active');
+  image.src = event.target.src;
+}
+
+function closeImage() {
+  container.classList.remove('fullscreen-image-container--active');
+}
+
+window.onload = () => {
+  document
+    .querySelectorAll('.markdown img')
+    .forEach((element) => element.addEventListener('click', openImage));
+
+  container.addEventListener('click', closeImage);
+
+  document.body.appendChild(container);
+};
