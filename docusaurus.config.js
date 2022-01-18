64765558e13a1bb5404bb739210c2de84ca51839
lucastortora <lucas.tortora@iota.org>
2022-01-18 09:25:31 -0300

    Revert "* Added integration services to megamenu"
    This reverts commit e20e869b5d51138ea7cf16033e17fa13b459f726.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,6 @@ module.exports = {
           layout: [
             '0 1 3 4',
             '0 1 3 4',
-            '0 1 3 2',
             '. 1 3 2',
             '. 1 3 2',
             '. 1 3 2',
@@ -135,13 +134,6 @@ module.exports = {
                   to: 'chronicle.rs/welcome',
                   icon: '\ue90c',
                   activeBaseRegex: '^/chronicle.rs/.*',
-                },
-                {
-                  label: 'Integration Services',
-                  sublabel: 'Integrate IOTA seamlessly',
-                  to: 'integration-services/welcome',
-                  icon: '\ue90d',
-                  activeBaseRegex: '^/integration-services/.*',
                 },
               ],
             },
