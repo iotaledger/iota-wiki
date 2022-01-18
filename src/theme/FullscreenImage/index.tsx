8556cebe869a7960b5b13f1effe961b4f9ccec6b
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-01-18 02:04:50 +0100

    Fix context signature full screen image provider

--- src/theme/FullscreenImage/index.tsx
+++ src/theme/FullscreenImage/index.tsx
@@ -1,14 +1,13 @@
 import React, { useState, useContext, createContext } from 'react';
 import './styles.css';
 
-const Context = createContext(() => {});
+const Context = createContext((url : string) => {});
 
 // eslint-disable-next-line react/prop-types
 function Provider({ children }) {
-  const [imageProps, setImageProps] = useState();
+  const [imageProps, setImageProps] = useState('');
 
   return (
-    // @ts-expect-error ts-migrate(2322) FIXME: Type 'Dispatch<(prevState: undefined) => undefined... Remove this comment to see the full error message
     <Context.Provider value={setImageProps}>
       {children}
       {imageProps && (
@@ -30,7 +29,6 @@ function Source(props) {
     <img
       {...props}
       className='fullscreen-image__source'
-      // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 1.
       onClick={() => setImageProps(props)}
     />
   );
