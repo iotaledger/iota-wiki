3cd3b3ef85588f34be15d476908cbb2fc31fa6e9
Critical <53269239+Critical94@users.noreply.github.com>
2022-02-02 10:03:58 +0100

    Add privacy policy and impressum (#455)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -344,6 +344,14 @@ module.exports = {
               label: 'Contribute',
               to: 'participate/contribute-to-wiki/welcome',
             },
+            {
+              label: 'Impressum',
+              to: 'https://www.iota.org/impressum',
+            },
+            {
+              label: 'Privacy Policy',
+              to: 'https://www.iota.org/privacy-policy',
+            },
           ],
         },
       ],
