99b2cf987d92261dec97b23a5d53143755384e20
Dr-Electron <dr-electr0n@protonmail.com>
2022-02-05 00:07:13 +0100

    Add more example tutorials and example tags

--- src/data/users.tsx
+++ src/data/users.tsx
@@ -43,7 +43,11 @@ export type TagType =
     | 'scam'
     | 'catlover'
     | 'silkysmoth'
-    | 'meme';
+    | 'meme'
+    | 'videotutorial'
+    | 'livecoding'
+    | 'texttutorial'
+    | 'identity';
 
 export type User = {
     title: string;
@@ -67,30 +71,54 @@ export const Tags: Record<TagType, Tag> = {
         color: '#e9669e',
     },
 
+    videotutorial: {
+        label: 'Video Tutorial',
+        description: 'Tutorial in video format',
+        color: '#39ca30',
+    },
+
+    texttutorial: {
+        label: 'Text Tutorial',
+        description: 'Tutorial in written format',
+        color: '#dfd545',
+    },
+
+    livecoding: {
+        label: 'Live Coding Session',
+        description: 'Recorded live coding session',
+        color: '#dfd545',
+    },
+
+    identity: {
+        label: 'IOTA Identity',
+        description: 'This Tutorial is using the IOTA Identity framework',
+        color: '#a44fb7',
+    },
+
     scam: {
         label: 'Scam',
         description: 'This seems fishy',
-        color: '#39ca30',
+        color: '#127f82',
     },
 
     catlover: {
         label: 'Cat-Lover',
         description: 'We love Cats(not the movie)!',
-        color: '#dfd545',
+        color: '#fe6829',
     },
 
     silkysmoth: {
         label: 'Silky Smoth',
         description:
             'Only one person can make hair silky smoth',
-        color: '#a44fb7',
+        color: '#4267b2',
     },
 
     meme: {
         label: 'Meme',
         description:
             'That\'s what we are here for, right?',
-        color: '#127f82',
+        color: '#14cfc3',
     },
 };
 
@@ -98,13 +126,13 @@ export const Tags: Record<TagType, Tag> = {
 // prettier-ignore
 const Users: User[] = [
     {
-        title: 'Hair like Jeroen',
+        title: 'Digitally Validate a Degree',
         description:
-            'Ever wanted to know how to get fabulous hair? Search no longer. In this tutorial we will show you how to style your hair perfectly',
-        preview: require('./tutorials/the-weekend.png'),
-        website: 'https://www.youtube.com/watch?v=E1TB4USszBk',
-        source: null,
-        tags: ['favorite', 'silkysmoth'],
+            'In this tutorial, you will use the WASM binding of the IOTA Identity framework to digitally prove the existence and validity of a degree.',
+        preview: require('./tutorials/validate-a-degree.png'),
+        website: 'https://dr-electron.github.io/iota-wiki/identity.rs/tutorials/validate_university_degree',
+        source: 'https://github.com/adrian-grassl/iota-identity-tutorial/',
+        tags: ['favorite', 'identity', 'texttutorial'],
     },
     {
         title: 'Cats, Cats and Cats (not the movie)',
@@ -116,6 +144,15 @@ const Users: User[] = [
         tags: ['favorite', 'catlover', 'meme'],
     },
     {
+        title: 'Hair like Jeroen',
+        description:
+            'Ever wanted to know how to get fabulous hair? Search no longer. In this tutorial we will show you how to style your hair perfectly',
+        preview: require('./tutorials/the-weekend.png'),
+        website: 'https://www.youtube.com/watch?v=E1TB4USszBk',
+        source: null,
+        tags: ['favorite', 'silkysmoth'],
+    },
+    {
         title: 'He is still 25 because of this little trick the beauty industry doesn\'t want you to know',
         description:
             'He went diving and came back as a 25 year old. How? Click here to learn',
@@ -123,6 +160,33 @@ const Users: User[] = [
         website: 'https://www.youtube.com/watch?v=Mh0uMbO78OM',
         source: null,
         tags: ['favorite', 'scam'],
+    },
+    {
+        title: 'ISCP Tutorials by Kumar Anirudha',
+        description:
+            'Youtube playlist with Smart Contract tutorials',
+        preview: require('./tutorials/iscp-tutorials.png'),
+        source: null,
+        website: 'https://www.youtube.com/playlist?list=PLMbc46iGTB_SCQ-Ut_c8__ssMDFUusVBJ',
+        tags: ['videotutorial'],
+    },
+    {
+        title: 'Rust Live Coding Sessions with the Bee Team',
+        description:
+            '25 Live coding sessions where you can follow some of the developments in the L1 Bee node',
+        preview: require('./tutorials/rust-live-coding-bee.png'),
+        website: 'https://www.youtube.com/playlist?list=PLMbc46iGTB_SBwcE5WfUIV5HthmH34U2R',
+        source: null,
+        tags: ['livecoding'],
+    },
+    {
+        title: 'Open SSH Key Generation Linux and MacOS',
+        description:
+            'Learn hot to secure your server access with SSH keys',
+        preview: require('./tutorials/openssh-generation.png'),
+        website: 'https://www.youtube.com/watch?v=Cbuch9_NeYo&list=PLMbc46iGTB_RUu-Kd2EIUAc8lQShfaCD6',
+        source: null,
+        tags: ['videotutorial'],
     },
     /*
     Pro Tip: add your tutorial in alphabetical order.
