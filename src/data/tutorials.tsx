d9e4b401a3dbd82039ee05382868402dab34fbc9
Dr-Electron <dr-electr0n@protonmail.com>
2022-02-06 00:14:59 +0100

    Add integration services

--- src/data/tutorials.tsx
+++ src/data/tutorials.tsx
@@ -33,97 +33,98 @@ import { sortBy } from '@site/src/utils/jsUtils';
  */
 
 export type Tag = {
-    label: string;
-    description: string;
-    color: string;
+  label: string;
+  description: string;
+  color: string;
 };
 
 export type TagType =
-    | 'favorite'
-    | 'scam'
-    | 'catlover'
-    | 'silkysmoth'
-    | 'meme'
-    | 'videotutorial'
-    | 'livecoding'
-    | 'texttutorial'
-    | 'identity'
-    | 'gettingstarted';
+  | 'favorite'
+  | 'scam'
+  | 'catlover'
+  | 'silkysmoth'
+  | 'meme'
+  | 'videotutorial'
+  | 'livecoding'
+  | 'texttutorial'
+  | 'identity'
+  | 'gettingstarted'
+  | 'integrationservices';
 
 export type Tutorial = {
-    title: string;
-    description: string;
-    preview: string;
-    website: string;
-    source: string | null;
-    tags: TagType[];
+  title: string;
+  description: string;
+  preview: string;
+  website: string;
+  source: string | null;
+  tags: TagType[];
 };
 
 const colors: string[] = [
-    '#010067',
-    '#d5ff00',
-    '#ff0056',
-    '#9e008e',
-    '#0e4ca1',
-    '#ffe502',
-    '#005f39',
-    '#00ff00',
-    '#95003a',
-    '#ff937e',
-    '#a42400',
-    '#001544',
-    '#91d0cb',
-    '#620e00',
-    '#6b6882',
-    '#0000ff',
-    '#007db5',
-    '#6a826c',
-    '#00ae7e',
-    '#c28c9f',
-    '#be9970',
-    '#008f9c',
-    '#5fad4e',
-    '#ff0000',
-    '#ff00f6',
-    '#ff029d',
-    '#683d3b',
-    '#ff74a3',
-    '#968ae8',
-    '#98ff52',
-    '#a75740',
-    '#01fffe',
-    '#ffeee8',
-    '#fe8900',
-    '#bdc6ff',
-    '#01d0ff',
-    '#bb8800',
-    '#7544b1',
-    '#a5ffd2',
-    '#ffa6fe',
-    '#774d00',
-    '#7a4782',
-    '#263400',
-    '#004754',
-    '#43002c',
-    '#b500ff',
-    '#ffb167',
-    '#ffdb66',
-    '#90fb92',
-    '#7e2dd2',
-    '#bdd393',
-    '#e56ffe',
-    '#deff74',
-    '#00ff78',
-    '#009bff',
-    '#006401',
-    '#0076ff',
-    '#85a900',
-    '#00b917',
-    '#788231',
-    '#00ffc6',
-    '#ff6e41',
-    '#e85ebe'
-]
+  '#010067',
+  '#d5ff00',
+  '#ff0056',
+  '#9e008e',
+  '#0e4ca1',
+  '#ffe502',
+  '#005f39',
+  '#00ff00',
+  '#95003a',
+  '#ff937e',
+  '#a42400',
+  '#001544',
+  '#91d0cb',
+  '#620e00',
+  '#6b6882',
+  '#0000ff',
+  '#007db5',
+  '#6a826c',
+  '#00ae7e',
+  '#c28c9f',
+  '#be9970',
+  '#008f9c',
+  '#5fad4e',
+  '#ff0000',
+  '#ff00f6',
+  '#ff029d',
+  '#683d3b',
+  '#ff74a3',
+  '#968ae8',
+  '#98ff52',
+  '#a75740',
+  '#01fffe',
+  '#ffeee8',
+  '#fe8900',
+  '#bdc6ff',
+  '#01d0ff',
+  '#bb8800',
+  '#7544b1',
+  '#a5ffd2',
+  '#ffa6fe',
+  '#774d00',
+  '#7a4782',
+  '#263400',
+  '#004754',
+  '#43002c',
+  '#b500ff',
+  '#ffb167',
+  '#ffdb66',
+  '#90fb92',
+  '#7e2dd2',
+  '#bdd393',
+  '#e56ffe',
+  '#deff74',
+  '#00ff78',
+  '#009bff',
+  '#006401',
+  '#0076ff',
+  '#85a900',
+  '#00b917',
+  '#788231',
+  '#00ffc6',
+  '#ff6e41',
+  '#e85ebe',
+];
 
 // LIST OF AVAILABLE TAGS
 // Available tags to assign to your tutorial
@@ -131,69 +132,73 @@ const colors: string[] = [
 // We'll remove inappropriate tags, but it's less likely that we add tags.
 // Colors for the tags are used from the 64 colors set here: http://godsnotwheregodsnot.blogspot.com/2012/09/color-distribution-methodology.html
 export const Tags: Record<TagType, Tag> = {
-    // DO NOT USE THIS TAG: we choose tutorials to add to favorites
-    favorite: {
-        label: 'Favorite',
-        description:
-            'Our favorite IOTA tutorials that you must absolutely check-out!',
-        color: colors[0],
-    },
+  // DO NOT USE THIS TAG: we choose tutorials to add to favorites
+  favorite: {
+    label: 'Favorite',
+    description:
+      'Our favorite IOTA tutorials that you must absolutely check-out!',
+    color: colors[0],
+  },
 
-    gettingstarted: {
-        label: 'Getting Started',
-        description: 'Easy Tutorials for getting started',
-        color: colors[1],
-    },
+  gettingstarted: {
+    label: 'Getting Started',
+    description: 'Easy Tutorials for getting started',
+    color: colors[1],
+  },
 
-    videotutorial: {
-        label: 'Video Tutorial',
-        description: 'Tutorial in video format',
-        color: colors[2],
-    },
+  videotutorial: {
+    label: 'Video Tutorial',
+    description: 'Tutorial in video format',
+    color: colors[2],
+  },
 
-    texttutorial: {
-        label: 'Text Tutorial',
-        description: 'Tutorial in written format',
-        color: colors[3],
-    },
+  texttutorial: {
+    label: 'Text Tutorial',
+    description: 'Tutorial in written format',
+    color: colors[3],
+  },
 
-    livecoding: {
-        label: 'Live Coding Session',
-        description: 'Recorded live coding session',
-        color: colors[4],
-    },
+  livecoding: {
+    label: 'Live Coding Session',
+    description: 'Recorded live coding session',
+    color: colors[4],
+  },
 
-    identity: {
-        label: 'IOTA Identity',
-        description: 'This Tutorial is using the IOTA Identity framework',
-        color: colors[5],
-    },
+  integrationservices: {
+    label: 'IOTA Integration Services',
+    description: 'Integrate IOTA seamlessly',
+    color: colors[5],
+  },
 
-    scam: {
-        label: 'Scam',
-        description: 'This seems fishy',
-        color: colors[6],
-    },
+  identity: {
+    label: 'IOTA Identity',
+    description: 'This Tutorial is using the IOTA Identity framework',
+    color: colors[6],
+  },
 
-    catlover: {
-        label: 'Cat-Lover',
-        description: 'We love Cats(not the movie)!',
-        color: colors[7],
-    },
+  scam: {
+    label: 'Scam',
+    description: 'This seems fishy',
+    color: colors[7],
+  },
 
-    silkysmoth: {
-        label: 'Silky Smoth',
-        description:
-            'Only one person can make hair silky smoth',
-        color: colors[8],
-    },
+  catlover: {
+    label: 'Cat-Lover',
+    description: 'We love Cats(not the movie)!',
+    color: colors[8],
+  },
 
-    meme: {
-        label: 'Meme',
-        description:
-            'That\'s what we are here for, right?',
-        color: colors[9],
-    },
+  silkysmoth: {
+    label: 'Silky Smoth',
+    description: 'Only one person can make hair silky smoth',
+    color: colors[9],
+  },
+
+  meme: {
+    label: 'Meme',
+    description: "That's what we are here for, right?",
+    color: colors[10],
+  },
 };
 
 // Add your tutorial to this list
@@ -204,7 +209,7 @@ const Tutorials: Tutorial[] = [
         description:
             'In this tutorial, you will use the WASM binding of the IOTA Identity framework to digitally prove the existence and validity of a degree.',
         preview: require('./tutorials/validate-a-degree.png'),
-        website: 'https://dr-electron.github.io/iota-wiki/identity.rs/tutorials/validate_university_degree',
+        website: 'identity.rs/tutorials/validate_university_degree',
         source: 'https://github.com/adrian-grassl/iota-identity-tutorial/',
         tags: ['favorite', 'identity', 'texttutorial'],
     },
@@ -254,6 +259,15 @@ const Tutorials: Tutorial[] = [
         tags: ['livecoding'],
     },
     {
+        title: 'Streams and Identity',
+        description:
+            'The IOTA Integration Services implement a high-level API for common interactions with Decentralized Identities and Data Streams based on the IOTA Identity and IOTA Streams libraries, facilitating the integration of IOTA into existing digital infrastructure.',
+        preview: require('./tutorials/integration-streams-identity.png'),
+        website: 'integration-services/welcome',
+        source: 'https://github.com/iotaledger/integration-services',
+        tags: ['integrationservices', 'texttutorial'],
+    },
+    {
         title: 'Open SSH Key Generation Linux and MacOS',
         description:
             'Learn hot to secure your server access with SSH keys',
@@ -261,6 +275,15 @@ const Tutorials: Tutorial[] = [
         website: 'https://www.youtube.com/watch?v=Cbuch9_NeYo&list=PLMbc46iGTB_RUu-Kd2EIUAc8lQShfaCD6',
         source: null,
         tags: ['videotutorial'],
+    },
+    {
+        title: 'Zebra Tutorial',
+        description:
+            'Use Zebra with IOTA DID',
+        preview: require('./tutorials/zebra.png'),
+        website: 'zebra-iota-edge-sdk/tutorials/101/zebra-iota-edge-sdk-101-tutorial',
+        source: 'https://github.com/dr-electron/zebra-iota-edge-sdk',
+        tags: ['integrationservices', 'texttutorial'],
     },
     /*
     Pro Tip: add your tutorial in alphabetical order.
@@ -270,12 +293,12 @@ const Tutorials: Tutorial[] = [
 
 export const TagList = Object.keys(Tags) as TagType[];
 function sortTutorials() {
-    let result = Tutorials;
-    // Sort by tutorial name
-    result = sortBy(result, (tutorial) => tutorial.title.toLowerCase());
-    // Sort by favorite tag, favorites first
-    result = sortBy(result, (tutorial) => !tutorial.tags.includes('favorite'));
-    return result;
+  let result = Tutorials;
+  // Sort by tutorial name
+  result = sortBy(result, (tutorial) => tutorial.title.toLowerCase());
+  // Sort by favorite tag, favorites first
+  result = sortBy(result, (tutorial) => !tutorial.tags.includes('favorite'));
+  return result;
 }
 
 export const sortedTutorials = sortTutorials();
