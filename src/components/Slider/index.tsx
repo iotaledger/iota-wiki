e68ad29f79576bb4fe7e6879d2de3185709bd913
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-02-13 10:24:17 +0100

    Resolve images from directory request

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -2,62 +2,75 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import axios from 'axios';
 
 export interface ImageSliderProps {
   path: string;
 }
 
 export default function ImageSlider({ path }: ImageSliderProps) {
-  // Import images from the infographics folder
-  function importImages(r) {
-    return r.keys().map((x) => x.replace('.', ''));
-  }
-
-  const allImages = importImages(
-    require.context('/img/', true, /\.(png|jpe?g|svg|mp4)$/),
+  const directory = useBaseUrl('/img' + path);
+  const videoPlaceholder = useBaseUrl(
+    '/img/infographics/video-placeholder.png',
   );
-  const requestedImages = allImages.filter((word) => word.startsWith(path));
+  const supportedImageExtensions = /(png|jpe?g|svg)$/;
+  const [images, setImages] = useState([]);
 
-  // Get file extension
   function getFileExtension(filename) {
     return filename.split('.').pop();
   }
 
-  // Create an array of objects with the image paths
-  function createImageArray() {
-    const images = [];
-    for (let i = 0; i < requestedImages.length; i++) {
-      const image = useBaseUrl('/img' + requestedImages[i]);
-      if (getFileExtension(requestedImages[i]) === 'mp4') {
-        images.push({
-          original: image,
-          thumbnail: useBaseUrl('/img/infographics/video-placeholder.png'),
-          renderItem: () => (
-            <video
-              controls
-              autoPlay={true}
-              muted
-              className='image-gallery-video'
-            >
-              <source src={image} type='video/mp4' />
-            </video>
-          ),
-        });
-      } else {
-        images.push({
-          original: image,
-          thumbnail: image,
-          thumbnailHeight: 48,
-        });
-      }
-    }
-    return images;
-  }
-  const carousel = useRef(null);
-  const images = createImageArray();
+  useEffect(() => {
+    const abortController = new AbortController();
+    axios
+      .get(directory, {
+        signal: abortController.signal,
+      })
+      .then(({ data: fileNames }) => {
+        const images = fileNames
+          .map((fileName: string) => {
+            const filePath = directory + '/' + fileName;
+            const fileExtension = getFileExtension(filePath);
+            let file = null;
 
-  // Create the image gallery
+            if (fileExtension === 'mp4') {
+              file = {
+                original: filePath,
+                thumbnail: videoPlaceholder,
+                renderItem: () => (
+                  <video
+                    controls
+                    autoPlay={true}
+                    muted
+                    className='image-gallery-video'
+                  >
+                    <source src={filePath} type='video/mp4' />
+                  </video>
+                ),
+              };
+            } else if (supportedImageExtensions.test(fileExtension)) {
+              file = {
+                original: filePath,
+                thumbnail: filePath,
+                thumbnailHeight: 48,
+              };
+            }
+
+            return file;
+          })
+          .filter((file: boolean) => file);
+
+        setImages(images);
+      });
+
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  const carousel = useRef(null);
+
   return (
     <ImageGallery
       onClick={() => carousel?.current?.fullScreen()}
