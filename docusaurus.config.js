b8f094f48d07b0f259b34228b2e5967742d0cb64
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-03-12 11:09:17 +0100

    Exclude thumbnails as well

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,8 @@ module.exports = {
       applicationId: '2809821',
     },
     imageZoom: {
-      selector: '.markdown :not(a) > img:not(.image-gallery-image)',
+      selector:
+        '.markdown :not(a) > img:not(.image-gallery-image):not(.image-gallery-thumbnail-image)',
       // Optional medium-zoom options
       // see: https://www.npmjs.com/package/medium-zoom#options
       options: {
