b108dd921b08a0bd7f243dfccf2bcf5d2b62fc6c
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-03-23 15:25:57 +0100

    Define steps to be taken by start

--- cli/src/commands/start.ts
+++ cli/src/commands/start.ts
@@ -1,15 +1,16 @@
 import { Command } from '@oclif/core';
 
 export default class Start extends Command {
-  static description = 'Start a development server to view your content.';
+  static description = 'Start a development server to preview your content.';
 
   public async run(): Promise<void> {
     this.log(
-      'TODO:' +
-        '- Check if a .wiki directory exists or create it otherwise.' +
-        '- Merge the Docusaurus config in the current directory with a Wiki Docusaurus config.' +
-        '- Place or replace the config in the .wiki directory.' +
-        '- Run Docusaurus from the .wiki directory.',
+      'TODO:\n' +
+        '[ ] Find the module config file in the Wiki cli siteDir.\n' +
+        '[ ] Set WIKI_MODULE_CONFIG to the path of the module config.\n' +
+        '[ ] Optionally set WIKI_PORT to what is configured using the port flag.\n' +
+        '[ ] Use the module config inside of the Wiki CLI internal config.\n' +
+        '[ ] Call docusaurus start using the internal config and the WIKI_PORT.\n',
     );
   }
 }
--- cli/test/commands/start.test.ts
+++ cli/test/commands/start.test.ts
@@ -4,16 +4,21 @@ describe('start', () => {
   test
     .stdout()
     .command(['start'])
-    .it(
-      'starts a development server using the Docusaurus config in the current directory',
-      (ctx) => {
-        expect(ctx.stdout).to.contain(
-          'TODO:' +
-            '- Check if a .wiki directory exists or create it otherwise.' +
-            '- Merge the Docusaurus config in the current directory with a Wiki Docusaurus config.' +
-            '- Place or replace the config in the .wiki directory.' +
-            '- Run Docusaurus from the .wiki directory.',
-        );
-      },
-    );
+    .it('starts a server correctly', (ctx) => {
+      expect(ctx.stdout).to.contain(
+        '[ ] Find the module config file in the Wiki cli siteDir.',
+      );
+      expect(ctx.stdout).to.contain(
+        '[ ] Set WIKI_MODULE_CONFIG to the path of the module config.',
+      );
+      expect(ctx.stdout).to.contain(
+        '[ ] Optionally set WIKI_PORT to what is configured using the port flag.',
+      );
+      expect(ctx.stdout).to.contain(
+        '[ ] Use the module config inside of the Wiki CLI internal config.',
+      );
+      expect(ctx.stdout).to.contain(
+        '[ ] Call docusaurus start using the internal config and the WIKI_PORT.',
+      );
+    });
 });
