485ff6bc7721ae5fe653fcae06b5b401077abff0
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-03-26 12:54:04 +0100

    Remove anything oclif specific

--- cli/test/commands/start.test.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@oclif/test';
-
-describe('start', () => {
-  test
-    .stdout()
-    .command(['start', '--port=3001', '--no-open', './test/module'])
-    .it('starts a server correctly');
-});
