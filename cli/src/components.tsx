1d30007ebdcb05645bd486cc1435309b9c221b73
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-03-26 15:46:07 +0100

    Clean up build

--- cli/src/components.tsx
+++ cli/src/components.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Text } from 'ink';
 
-export const Base = () => <Text>Base command</Text>;
+export const Default = () => <Text>Default command</Text>;
 
 interface StartOptions {
   config: string;
 }
 
-export function Start({ config }: StartOptions) {
+export const Start: FC<StartOptions> = ({ config }) => {
   return <Text>Start subcommand with config {config}</Text>;
-}
+};
 
 export default {
-  Base,
+  Default,
   Start,
 };
--- cli/src/index.tsx
+++ cli/src/index.tsx
@@ -1,22 +1,24 @@
+#!/usr/bin/env node
+
 import React from 'react';
 import { render } from 'ink';
 import { Command, Option, runExit } from 'clipanion';
 import Components from './components';
 
-class Base extends Command {
+class Default extends Command {
   async execute() {
-    render(<Components.Base />);
+    render(<Components.Default />);
   }
 }
 
 class Start extends Command {
   static paths = [[`start`]];
 
-  config = Option.String();
+  config = Option.String('-c,--config', './docusaurus.config.js');
 
   async execute() {
     render(<Components.Start config={this.config} />);
   }
 }
 
-runExit([Base, Start]);
+runExit([Default, Start]);
