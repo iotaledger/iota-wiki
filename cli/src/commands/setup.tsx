2163f69e7d2baa9602937f00f15c4eb962dca188
Dr-Electron <dr-electr0n@protonmail.com>
2022-03-29 01:04:04 +0200

    Leave something for Jeroen to fix
    - Package is outdated. We need to use the git link directly

--- cli/src/commands/setup.tsx
+++ cli/src/commands/setup.tsx
@@ -1,4 +1,44 @@
 import { Command } from 'clipanion';
+import { render } from 'ink';
+import { Form, FormProps } from 'ink-form';
+import React from 'react';
+
+const tags = [{ value: 'tag1' }, { value: 'tag2' }];
+
+const form: FormProps = {
+  form: {
+    title: 'Tutorial Setup',
+    sections: [
+      {
+        title: '',
+        fields: [
+          { type: 'string', name: 'titel', label: 'Enter your tutorial title' },
+          {
+            type: 'string',
+            name: 'description',
+            label: 'Enter your tutorial description',
+          },
+          {
+            type: 'string',
+            name: 'preview',
+            label: 'Enter the name of your preview image',
+          },
+          {
+            type: 'string',
+            name: 'source',
+            label: 'Enter the url of your github repo',
+          },
+          {
+            type: 'multiselect',
+            name: 'tags',
+            label: 'Choose some tags',
+            options: tags,
+          },
+        ],
+      },
+    ],
+  },
+};
 
 export class Setup extends Command {
   static paths = [[`setup`]];
@@ -8,6 +48,14 @@ export class Setup extends Command {
   });
 
   async execute() {
-    this.context.stdout.write('Setup something!\n');
+    // or:
+    render(
+      <Form
+        form={form.form}
+        onSubmit={(result) => {
+          console.log(`Finished with value`, result);
+        }}
+      />,
+    );
   }
 }
