dc0303bd29d9cd9c46a1df4dacad19b10d2b8783
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-04-27 21:02:10 +0200

    Fix mega menu
    * Update mega menu components
    
    * Refactor NavbarNavlink
    
    * Re implement mega dropdown
    
    * Remove default item
    
    * Fix for Safari
    
    Co-authored-by: Dr-Electron <dr-electr0n@protonmail.com>

--- src/theme/NavbarItem/NavbarNavLink/index.tsx
+++ src/theme/NavbarItem/NavbarNavLink/index.tsx
@@ -1,19 +1,27 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import React from 'react';
 import Link from '@docusaurus/Link';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import isInternalUrl from '@docusaurus/isInternalUrl';
+import type { Props as OriginalProps } from '@theme/NavbarItem/NavbarNavLink';
 import IconExternalLink from '@theme/IconExternalLink';
-import type { Props as NavLinkProps } from '@theme/NavbarItem/DefaultNavbarItem';
+import isInternalUrl from '@docusaurus/isInternalUrl';
+import { isRegexpStringMatch } from '@docusaurus/theme-common';
 import './styles.css';
 
 const dropdownLinkActiveClass = 'dropdown__link--active';
 
-export interface ExtendedNavLinkProps extends NavLinkProps {
+export interface Props extends OriginalProps {
   sublabel?: string;
   icon?: string;
 }
 
-export default function NavLink({
+export default function NavbarNavLink({
   activeBasePath,
   activeBaseRegex,
   to,
@@ -21,10 +29,10 @@ export default function NavLink({
   label,
   sublabel,
   icon,
-  activeClassName = 'navbar__link--active',
+  activeClassName = '',
   prependBaseUrlToHref,
   ...props
-}: ExtendedNavLinkProps): JSX.Element {
+}: Props): JSX.Element {
   // TODO all this seems hacky
   // {to: 'version'} should probably be forbidden, in favor of {to: '/version'}
   const toUrl = useBaseUrl(to);
@@ -32,11 +40,6 @@ export default function NavLink({
   const normalizedHref = useBaseUrl(href, { forcePrependBaseUrl: true });
   const isExternalLink = label && href && !isInternalUrl(href);
   const isDropdownLink = activeClassName === dropdownLinkActiveClass;
-
-  const checkIsActive = (_match, location) =>
-    activeBaseRegex
-      ? new RegExp(activeBaseRegex).test(location.pathname)
-      : location.pathname.startsWith(activeBaseUrl);
 
   return (
     <Link
@@ -46,11 +49,16 @@ export default function NavLink({
           }
         : {
             isNavLink: true,
-            activeClassName,
+            activeClassName: !props.className?.includes(activeClassName)
+              ? activeClassName
+              : '',
             to: toUrl,
             ...(activeBasePath || activeBaseRegex
               ? {
-                  isActive: checkIsActive,
+                  isActive: (_match, location) =>
+                    activeBaseRegex
+                      ? isRegexpStringMatch(activeBaseRegex, location.pathname)
+                      : location.pathname.startsWith(activeBaseUrl),
                 }
               : null),
           })}
@@ -60,18 +68,14 @@ export default function NavLink({
         {icon && <div className='link__icon'>{icon}</div>}
         <div className='link__body'>
           <div className='link__label'>
-            {isExternalLink ? (
-              <span>
-                {label}
-                <IconExternalLink
-                  {...(isDropdownLink && {
-                    width: 12,
-                    height: 12,
-                  })}
-                />
-              </span>
-            ) : (
-              label
+            {label}
+            {isExternalLink && (
+              <IconExternalLink
+                {...(isDropdownLink && {
+                  width: 12,
+                  height: 12,
+                })}
+              />
             )}
           </div>
           {sublabel && <div className='link__sublabel'>{sublabel}</div>}
--- src/theme/NavbarItem/index.tsx
+++ src/theme/NavbarItem/index.tsx
@@ -11,56 +11,31 @@
  */
 
 import React from 'react';
-import type { ComponentProps } from 'react';
 import DefaultNavbarItem from '@theme/NavbarItem/DefaultNavbarItem';
-import DropdownNavbarItem from '@theme/NavbarItem/DropdownNavbarItem';
-import MegaDropdownNavbarItem from '@site/src/components/MegaDropdownNavbarItem';
+import DropdownNavbarItem, {
+  type Props as DropdownNavbarItemProps,
+} from '@theme/NavbarItem/DropdownNavbarItem';
+import MegaDropdownNavbarItem from '../../components/MegaDropdownNavbarItem';
 import LocaleDropdownNavbarItem from '@theme/NavbarItem/LocaleDropdownNavbarItem';
 import SearchNavbarItem from '@theme/NavbarItem/SearchNavbarItem';
-import type { LinkLikeNavbarItemProps } from '@theme/NavbarItem';
-import type { Props as DropdownNavbarItemProps } from '@theme/NavbarItem/DropdownNavbarItem';
-import type { Props as DocsVersionDropdownNavbarItemProps } from '@theme/NavbarItem/DocsVersionDropdownNavbarItem';
-import type { Props as LocaleDropdownNavbarItemProps } from '@theme/NavbarItem/LocaleDropdownNavbarItem';
-import type { Props as SearchNavbarItemProps } from '@theme/NavbarItem/SearchNavbarItem';
-import type { Props as MegaDropdownNavbarItemProps } from '@site/src/components/MegaDropdownNavbarItem';
+import type { Types, Props } from '@theme/NavbarItem';
 import './styles.css';
 
-type Props = ComponentProps<'a'> & {
-  readonly position?: 'left' | 'right';
-} & (
-    | LinkLikeNavbarItemProps
-    | ({ readonly type?: 'dropdown' } & DropdownNavbarItemProps)
-    | ({ readonly type: 'megaDropdown' } & MegaDropdownNavbarItemProps)
-    | ({
-        readonly type: 'docsVersionDropdown';
-      } & DocsVersionDropdownNavbarItemProps)
-    | ({ readonly type: 'localeDropdown' } & LocaleDropdownNavbarItemProps)
-    | ({
-        readonly type: 'search';
-      } & SearchNavbarItemProps)
-  );
-
-type Types = Props['type'];
-
-const NavbarItemComponents: Record<
-  Exclude<Types, undefined>,
-  // TODO: properly type this
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  () => (props: any) => JSX.Element
-> = {
+const NavbarItemComponents = {
   default: () => DefaultNavbarItem,
   localeDropdown: () => LocaleDropdownNavbarItem,
   search: () => SearchNavbarItem,
   dropdown: () => DropdownNavbarItem,
   megaDropdown: () => MegaDropdownNavbarItem,
-  // Need to lazy load these items as we don't know for sure the docs plugin is loaded
-  // See https://github.com/facebook/docusaurus/issues/3360
+
+  // Need to lazy load these items as we don't know for sure the docs plugin is
+  // loaded. See https://github.com/facebook/docusaurus/issues/3360
   /* eslint-disable @typescript-eslint/no-var-requires, global-require */
   docsVersion: () => require('@theme/NavbarItem/DocsVersionNavbarItem').default,
   docsVersionDropdown: () =>
     require('@theme/NavbarItem/DocsVersionDropdownNavbarItem').default,
   doc: () => require('@theme/NavbarItem/DocNavbarItem').default,
-  docSidebar: () => require('@theme/DocSidebar').default,
+  docSidebar: () => require('@theme/NavbarItem/DocSidebarNavbarItem').default,
   /* eslint-enable @typescript-eslint/no-var-requires, global-require */
 } as const;
 
@@ -74,24 +49,26 @@ const getNavbarItemComponent = (type: NavbarItemComponentType) => {
   return navbarItemComponentFn();
 };
 
-function getComponentType(type, props): NavbarItemComponentType {
-  if (props.layout !== undefined) {
-    return 'megaDropdown';
-  }
-
+function getComponentType(
+  type: Types,
+  props,
+  isDropdown: boolean,
+): NavbarItemComponentType {
+  if (props.layout !== undefined) return 'megaDropdown';
   // Backward compatibility: navbar item with no type set
-  // but containing dropdown items should use the type 'dropdown'
-  if (!props.type || props.type === 'default') {
-    return props.items !== undefined ? 'dropdown' : 'default';
+  // but containing dropdown items should use the type "dropdown"
+  if (!type || type === 'default') {
+    return isDropdown ? 'dropdown' : 'default';
   }
   return type as NavbarItemComponentType;
 }
 
-export const getInfimaActiveClassName = (mobile?: boolean): string =>
-  mobile ? 'menu__link--active' : 'navbar__link--active';
-
 export default function NavbarItem({ type, ...props }: Props): JSX.Element {
-  const componentType = getComponentType(type, props);
+  const componentType = getComponentType(
+    type,
+    props,
+    (props as DropdownNavbarItemProps).items !== undefined,
+  );
   const NavbarItemComponent = getNavbarItemComponent(componentType);
   return <NavbarItemComponent {...props} />;
 }
