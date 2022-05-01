69f22732cff573afb639fb7591e261056a833eaf
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-02 00:48:35 +0200

    Add new build logic
    * Implement basic template for new build system
    
    * Update yarn to 3.2.0
    
    * Ignore node modules and update build workspaces
    
    * Fix yarn.lock
    
    * Add submodule updating
    
    * Update yarn.lock
    
    * Add BUILD env var
    
    * Fix gh action
    
    * Update .gitignore
    
    Co-authored-by: Jeroen van den Hout <jlvandenhout@gmail.com>
    
    Co-authored-by: Jeroen van den Hout <jlvandenhout@gmail.com>

---
+++ content/build/docusaurus.config.js
@@ -0,0 +1,52 @@
+// @ts-check
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+var requireGlob = require('require-glob');
+
+const env = process.env.BUILD;
+
+const reducer = function (_options, result, fileObject) {
+    if (fileObject && fileObject.exports) {
+        const { plugins, staticDirectories } = fileObject.exports;
+
+        if (Object.keys(result).length === 0) {
+            result = {
+                plugins,
+                staticDirectories,
+            };
+        } else {
+            if (!result.plugins) result.plugins = plugins;
+            else result.plugins = result.plugins.concat(plugins);
+            if (!result.staticDirectories)
+                result.staticDirectories = staticDirectories;
+            else
+                result.staticDirectories =
+                    result.staticDirectories.concat(staticDirectories);
+        }
+    }
+
+    return result;
+};
+
+const config = requireGlob.sync(
+    [
+        env + '/docusaurus.config.js',
+        '!node_modules',
+    ],
+    {
+        reducer,
+    },
+);
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+    title: 'Build',
+    url: '/',
+    baseUrl: '/',
+    themes: ['@docusaurus/theme-classic'],
+    plugins: !config.plugins ? [] : [
+        ...config.plugins
+    ],
+    staticDirectories: !config.staticDirectories ? [] : [
+        ...config.staticDirectories
+    ],
+};
