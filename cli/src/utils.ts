eb46bd8065110c5cf46456483620056121274775
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-05-05 01:16:34 +0200

    Remove redundant code

--- cli/src/utils.ts
+++ cli/src/utils.ts
@@ -1,38 +1,5 @@
-import { writeFileSync } from 'fs';
 import { execute } from '@yarnpkg/shell';
 import { PassThrough } from 'stream';
-
-const createConfig = (data, id) =>
-  `const path = require('path');
-
-module.exports = {
-    title: '${data.title}',
-    url: '/',
-    baseUrl: '/',
-    themes: ['@docusaurus/theme-classic'],
-    tutorial: {
-        title: '${data.title}',
-        description: '${data.description}',
-        preview: './${data.preview}',
-        website: '${id}/${data.firstPage}',
-        source: '${data.sourceUrl}',
-        tags: ['${data.tags.join("','")}'],
-    },
-    plugins: [
-        [
-            '@docusaurus/plugin-content-docs',
-            {
-                id: '${id}',
-                path: path.resolve(__dirname, './docs'),
-                routeBasePath: '${id}',
-                sidebarPath: path.resolve(__dirname, './sidebars.js'),
-                editUrl: '${data.sourceUrl}/edit/main/',
-            },
-        ],
-    ],
-    staticDirectories: [path.resolve(__dirname, './images')],
-};
-`;
 
 export async function readCommandLine(command: string) {
   const chunks: Array<Buffer> = [];
