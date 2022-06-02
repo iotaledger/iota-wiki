2fe1317805bb343c65c28bae3d13d660d3669a9d
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-06-02 20:23:37 +0200

    Fix footer list styling

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,146 +1,36 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+import React from 'react';
+
+import {useThemeConfig} from '@docusaurus/theme-common';
+import FooterLinks from '@theme/Footer/Links';
+import FooterLogo from '@theme/Footer/Logo';
+import FooterCopyright from '@theme/Footer/Copyright';
+import FooterLayout from '@theme/Footer/Layout';
+import Social from '@site/src/components/Social';
+
 
 /**
- * REASONS TO SWIZZLE:
+ * SWIZZLED VERSION: 2.0.0-beta.20
+ * REASONS:
  * - The socials bar needed full width, so needed to be placed outside the default container.
  */
 
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import { FooterLinkItem, useThemeConfig } from '@docusaurus/theme-common';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import ThemedImage, { Props as ThemedImageProps } from '@theme/ThemedImage';
-import Social from '@site/src/components/Social';
-
-function FooterLink({
-  to,
-  href,
-  label,
-  prependBaseUrlToHref,
-  ...props
-}: FooterLinkItem) {
-  const toUrl = useBaseUrl(to);
-  const normalizedHref = useBaseUrl(href, { forcePrependBaseUrl: true });
-
-  return (
-    <Link
-      className='footer__link-item'
-      {...(href
-        ? {
-            href: prependBaseUrlToHref ? normalizedHref : href,
-          }
-        : {
-            to: toUrl,
-          })}
-      {...props}
-    >
-      {label}
-    </Link>
-  );
-}
-
-function FooterLogo({
-  sources,
-  alt,
-  width,
-  height,
-}: Pick<ThemedImageProps, 'sources' | 'alt' | 'width' | 'height'>) {
-  return (
-    <ThemedImage
-      className='footer__logo'
-      alt={alt}
-      sources={sources}
-      width={width}
-      height={height}
-    />
-  );
-}
-
 function Footer(): JSX.Element | null {
-  const { footer } = useThemeConfig();
-  const { copyright, links = [], logo = {} } = footer || {};
-  const sources = {
-    light: useBaseUrl(logo.src),
-    dark: useBaseUrl(logo.srcDark || logo.src),
-  };
-
+  const {footer} = useThemeConfig();
   if (!footer) {
     return null;
   }
+  const {copyright, links, logo, style} = footer;
 
   return (
-    <footer
-      className={clsx('footer', 'padding--none', {
-        'footer--dark': footer.style === 'dark',
-      })}
-    >
-      <div className='container'>
-        {links && links.length > 0 && (
-          <div className='row footer__links padding-vert--xl'>
-            {links.map((linkItem, i) => (
-              <div key={i} className='col footer__col'>
-                {linkItem.title != null ? (
-                  <h4 className='footer__title'>{linkItem.title}</h4>
-                ) : null}
-                {linkItem.items != null &&
-                Array.isArray(linkItem.items) &&
-                linkItem.items.length > 0 ? (
-                  <ul className='footer__items'>
-                    {linkItem.items.map((item, key) =>
-                      item.html ? (
-                        <li
-                          key={key}
-                          className='footer__item' // Developer provided the HTML, so assume it's safe.
-                          // eslint-disable-next-line react/no-danger
-                          dangerouslySetInnerHTML={{
-                            __html: item.html,
-                          }}
-                        />
-                      ) : (
-                        <li key={item.href || item.to} className='footer__item'>
-                          <FooterLink {...item} />
-                        </li>
-                      ),
-                    )}
-                  </ul>
-                ) : null}
-              </div>
-            ))}
-          </div>
-        )}
-        {(logo || copyright) && (
-          <div className='footer__bottom padding-bottom--xl'>
-            {logo && (logo.src || logo.srcDark) && (
-              <div>
-                {logo.href ? (
-                  <Link href={logo.href}>
-                    <FooterLogo alt={logo.alt} sources={sources} />
-                  </Link>
-                ) : (
-                  <FooterLogo alt={logo.alt} sources={sources} />
-                )}
-              </div>
-            )}
-            {copyright ? (
-              <div
-                className='footer__copyright' // Developer provided the HTML, so assume it's safe.
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: copyright,
-                }}
-              />
-            ) : null}
-          </div>
-        )}
-      </div>
+    <>
+      <FooterLayout
+        style={style}
+        links={links && links.length > 0 && <FooterLinks links={links} />}
+        logo={logo && <FooterLogo logo={logo} />}
+        copyright={copyright && <FooterCopyright copyright={copyright} />}
+      />
       <Social />
-    </footer>
+    </>
   );
 }
 
