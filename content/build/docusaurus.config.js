291b8b3d2ae73e8e53b81be1a8fa363fdd6a7358
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-06-07 15:28:57 +0200

    Allow empty BUILD variable, closes #561

--- content/build/docusaurus.config.js
+++ content/build/docusaurus.config.js
@@ -2,52 +2,26 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 var requireGlob = require('require-glob');
 
-const env = process.env.BUILD;
-const globs = env.split(',').map(item => `${item}/docusaurus.config.js`);
+const { BUILD = '' } = process.env;
+const configs = BUILD.split(',').map(item => `${item}/docusaurus.config.js`);
 
-const reducer = function (_options, result, fileObject) {
+function reducer(_options, result, fileObject) {
+    // TODO: pass default object after shannonmoeller/require-glob#24 is merged.
+    if (Object.keys(result).length === 0) result = { plugins: [], staticDirectories: [] };
+
     if (fileObject && fileObject.exports) {
-        const { plugins, staticDirectories } = fileObject.exports;
-
-        if (Object.keys(result).length === 0) {
-            result = {
-                plugins,
-                staticDirectories,
-            };
-        } else {
-            if (!result.plugins) result.plugins = plugins;
-            else result.plugins = result.plugins.concat(plugins);
-            if (!result.staticDirectories)
-                result.staticDirectories = staticDirectories;
-            else
-                result.staticDirectories =
-                    result.staticDirectories.concat(staticDirectories);
-        }
+        const { plugins = [], staticDirectories = []} = fileObject.exports;
+        result.plugins.push(...plugins);
+        result.staticDirectories.push(...staticDirectories);
     }
 
     return result;
 };
 
-const config = requireGlob.sync(
-    [
-        ...globs,
-        '!node_modules',
-    ],
-    {
-        reducer,
-    },
-);
+let config = requireGlob.sync([...configs, '!node_modules'], {reducer});
+
+// TODO: remove after shannonmoeller/require-glob#24 is merged.
+if (Object.keys(config).length === 0) config = { plugins: [], staticDirectories: [] };
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
-    title: 'Build',
-    url: '/',
-    baseUrl: '/',
-    themes: ['@docusaurus/theme-classic'],
-    plugins: !config.plugins ? [] : [
-        ...config.plugins
-    ],
-    staticDirectories: !config.staticDirectories ? [] : [
-        ...config.staticDirectories
-    ],
-};
+module.exports = config;
