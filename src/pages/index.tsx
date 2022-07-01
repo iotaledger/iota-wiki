74a7993c118b61b3f0adfc7bc90ec77277db82ce
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-07-01 20:43:29 +0200

    Start from scratch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import HomeLayout from '@site/src/components/HomeLayout';
+import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <HomeLayout
-      description={siteConfig.tagline}
-      title={siteConfig.title}
-      tagline={siteConfig.tagline}
-    />
+    <Layout description={siteConfig.tagline} >
+      
+    </Layout>
   );
 }
