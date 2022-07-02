347d297ca3003048b714396c9ae168f5f00d5f8d
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-07-02 23:04:39 +0200

    Refactor container

--- src/components/HomeLayout/index.tsx
+++ src/components/HomeLayout/index.tsx
@@ -11,19 +11,21 @@ export default function HomeLayout() {
     <Layout description={siteConfig.tagline} >
       <div className='homepage'>
         <div className='homepage__section homepage__section--gradient'>
-          <section className='homepage__container header'>
-            <div className='header__text'>
-              <h1 className='header__title'>The complete reference for IOTA</h1>
-              <p className='header__paragraph' >
-                Build apps capable of taking millions of users on journeys they’ve never been on before. Simple. Scalable. Secure.
-              </p>
-              <div className='header__buttons'>
-                <Link className='header__button button button--outline button--primary'>Learn about IOTA</Link>
-                <Link className='header__button button button--primary'>Start building</Link>
+          <div className='homepage__container'>
+            <div className='header'>
+              <div className='header__text'>
+                <h1 className='header__title'>The complete reference for IOTA</h1>
+                <p className='header__paragraph' >
+                  Build apps capable of taking millions of users on journeys they’ve never been on before. Simple. Scalable. Secure.
+                </p>
+                <div className='header__buttons'>
+                  <Link className='header__button button button--outline button--primary'>Learn about IOTA</Link>
+                  <Link className='header__button button button--primary'>Start building</Link>
+                </div>
               </div>
+              <img className='header__image' src='img/homepage/header.svg' />
             </div>
-            <img className='header__image' src='img/homepage/header.svg' />
-          </section>
+          </div>
         </div>
       </div>
     </Layout>
