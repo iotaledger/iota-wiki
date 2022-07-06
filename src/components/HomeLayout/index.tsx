c7dc4fe8381a22405a70e4c39e6547b8aa8e3c5e
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-07-06 15:06:19 +0200

    Update texts from Figma

--- src/components/HomeLayout/index.tsx
+++ src/components/HomeLayout/index.tsx
@@ -74,44 +74,50 @@ export default function HomeLayout() {
               <div className='about__section'>
                 <div className='about__icon'>{'\ue907'}</div>
                 <h3 className='about__header'>What is IOTA?</h3>
-                <p className='about__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit, pharetra proin quis egestas.</p>
+                <p className='about__description'>
+                  An open, feeless and scalable distributed ledger, designed to support data and value transfer.
+                </p>
               </div>
             </Link>
             <Link className='about__card'>
               <div className='about__section'>
                 <div className='about__icon'>{'\ue907'}</div>
                 <h3 className='about__header'>Get IOTA tokens</h3>
-                <p className='about__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p className='about__description'>
+                  The IOTA token (listed as MIOTA) is used to transact value on the IOTA distributed ledger. 
+                </p>
               </div>
             </Link>
             <Link className='about__card'>
               <div className='about__section'>
                 <div className='about__icon'>{'\ue90a'}</div>
                 <h3 className='about__header'>Pick a wallet</h3>
-                <p className='about__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p className='about__description'>
+                  Choose a wallet to store your public and private keys and interact with the IOTA network.
+                </p>
               </div>
             </Link>
           </div>
         </HomepageSection>
         <HomepageSection
           header='Shimmer & IOTA'
-          description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit, pharetra proin quis egestas risus augue ante justo. Pharetra proin quis egestas risus augue ante justo.'
+          description='Shimmer is the official incentivized staging network of IOTA. All major upgrades of IOTA are first made available on the Shimmer network, offering builders and users a dedicated network to get early access to new innovations and opportunities.'
         >
           <div className='networks__cards'>
             <div className='networks__card'>
               <div className='networks__section'>
                 <div className='networks__icon networks__icon--shimmer'>{'\ue907'}</div>
                 <span className='networks__label'>Experimental</span>
-                <h2 className='networks__header'>Shimmer Network</h2>
+                <h2 className='networks__header'>Shimmer Stagenet</h2>
                 <p className='networks__description'>
                   Build and test your project.<br/>Port it later on to the mainnet.
                 </p>
               </div>
               <div className='networks__section'>
                 <ul className='networks__features'>
-                  <li className='networks__feature networks__feature--done'>Lorem ipsum ipsum</li>
-                  <li className='networks__feature networks__feature--done'>Lorem ipsum</li>
-                  <li className='networks__feature networks__feature--done'>Lorem ipsum Lorem ipsum</li>
+                  <li className='networks__feature networks__feature--done'>Staking</li>
+                  <li className='networks__feature networks__feature--done'>Permissionless</li>
+                  <li className='networks__feature networks__feature--done'>Newest features</li>
                 </ul>
                 <Link className='networks__button button button--outline button--primary'>
                   Learn more
@@ -124,14 +130,14 @@ export default function HomeLayout() {
                 <span className='networks__label'>Stable</span>
                 <h2 className='networks__header'>IOTA Mainnet</h2>
                 <p className='networks__description'>
-                  Sit amet, consectetur adipiscing elit. Augue faucibus pellentesque convallis magna.
+                  Mainnet is the IOTA network that uses the IOTA tokens. This network is the most stable.
                 </p>
               </div>
               <div className='networks__section'>
                 <ul className='networks__features'>
-                  <li className='networks__feature networks__feature--done'>Lorem ipsum ipsum</li>
-                  <li className='networks__feature'>To be announced</li>
-                  <li className='networks__feature'>To be announced</li>
+                  <li className='networks__feature networks__feature--done'>Permissionless</li>
+                  <li className='networks__feature'>DeFi enabled</li>
+                  <li className='networks__feature'>DApps and DAOs</li>
                 </ul>
                 <Link className='networks__button button button--outline button--primary'>
                   Learn more
@@ -140,7 +146,7 @@ export default function HomeLayout() {
             </div>
           </div>
         </HomepageSection>
-        <HomepageSection header='Start Building'>
+        <HomepageSection header='Start building'>
           <div className='start-building'>
             <div className='start-building__section'>
               <h2 className='start-building__header'>IOTA Smart Contracts</h2>
@@ -164,7 +170,8 @@ export default function HomeLayout() {
                 <div className='libraries__head'>
                   <div className='libraries__icon'>{'\ue907'}</div>
                   <Languages languages={{
-                    'rust': '/',
+                    'Rust': '/',
+                    'WASM': '/',
                   }}/>
                 </div>
                 <h3 className='libraries__header'>IDENTITY</h3>
@@ -183,21 +190,21 @@ export default function HomeLayout() {
                 <div className='libraries__head'>
                   <div className='libraries__icon'>{'\ue907'}</div>
                   <Languages languages={{
-                    'rust': '/',
-                    'c': '/',
-                    'python': '/',
-                    'java': '/',
-                    'wasm': '/',
+                    'Rust': '/',
+                    'NodeJS': '/',
+                    'Python': '/',
+                    'Java': '/',
+                    'WASM': '/',
                   }}/>
                 </div>
-                <h3 className='libraries__header'>IDENTITY</h3>
+                <h3 className='libraries__header'>CLIENT</h3>
                 <ul className='libraries__features'>
-                  <li className='libraries__feature'>Identity framework</li>
-                  <li className='libraries__feature'>Decentralized Identifiers (DID)</li>
-                  <li className='libraries__feature'>Verifiable Credentials (VC)</li>
+                  <li className='libraries__feature'>Interact with the IOTA network</li>
+                  <li className='libraries__feature'>Create messages and transactions</li>
+                  <li className='libraries__feature'>Sign transactions</li>
                 </ul>
                 <Link className='libraries__button button button--outline button--primary'>
-                  Create an identity
+                  Interact with the network
                 </Link>
               </div>
             </div>
@@ -206,17 +213,20 @@ export default function HomeLayout() {
                 <div className='libraries__head'>
                   <div className='libraries__icon'>{'\ue907'}</div>
                   <Languages languages={{
-                    'rust': '/',
+                    'Rust': '/',
+                    'NodeJS': '/',
+                    'Python': '/',
+                    'WASM': '/',
                   }}/>
                 </div>
-                <h3 className='libraries__header'>IDENTITY</h3>
+                <h3 className='libraries__header'>WALLET</h3>
                 <ul className='libraries__features'>
-                  <li className='libraries__feature'>Identity framework</li>
-                  <li className='libraries__feature'>Decentralized Identifiers (DID)</li>
-                  <li className='libraries__feature'>Verifiable Credentials (VC)</li>
+                  <li className='libraries__feature'>Integrate an IOTA wallet</li>
+                  <li className='libraries__feature'>Store account data</li>
+                  <li className='libraries__feature'>State management and backup</li>
                 </ul>
                 <Link className='libraries__button button button--outline button--primary'>
-                  Create an identity
+                  Integrate a wallet
                 </Link>
               </div>
             </div>
@@ -225,17 +235,19 @@ export default function HomeLayout() {
                 <div className='libraries__head'>
                   <div className='libraries__icon'>{'\ue907'}</div>
                   <Languages languages={{
-                    'rust': '/',
+                    'Rust': '/',
+                    'WASM': '/',
+                    'C': '/',
                   }}/>
                 </div>
-                <h3 className='libraries__header'>IDENTITY</h3>
+                <h3 className='libraries__header'>STREAMS</h3>
                 <ul className='libraries__features'>
-                  <li className='libraries__feature'>Identity framework</li>
-                  <li className='libraries__feature'>Decentralized Identifiers (DID)</li>
-                  <li className='libraries__feature'>Verifiable Credentials (VC)</li>
+                  <li className='libraries__feature'>Organize and share data securely</li>
+                  <li className='libraries__feature'>Integrity and authenticity</li>
+                  <li className='libraries__feature'>Control the ownership of data</li>
                 </ul>
                 <Link className='libraries__button button button--outline button--primary'>
-                  Create an identity
+                  Try out Streams
                 </Link>
               </div>
             </div>
@@ -244,17 +256,17 @@ export default function HomeLayout() {
                 <div className='libraries__head'>
                   <div className='libraries__icon'>{'\ue907'}</div>
                   <Languages languages={{
-                    'rust': '/',
+                    'Rust': '/',
                   }}/>
                 </div>
-                <h3 className='libraries__header'>IDENTITY</h3>
+                <h3 className='libraries__header'>STRONGHOLD</h3>
                 <ul className='libraries__features'>
-                  <li className='libraries__feature'>Identity framework</li>
-                  <li className='libraries__feature'>Decentralized Identifiers (DID)</li>
-                  <li className='libraries__feature'>Verifiable Credentials (VC)</li>
+                  <li className='libraries__feature'>Secure management of passwords</li>
+                  <li className='libraries__feature'>Secure personal data and private keys</li>
+                  <li className='libraries__feature'>Isolating digital secrets</li>
                 </ul>
                 <Link className='libraries__button button button--outline button--primary'>
-                  Create an identity
+                  Get to know Stronghold
                 </Link>
               </div>
             </div>
@@ -269,19 +281,16 @@ export default function HomeLayout() {
               <div className='nodes__icon'>{'\ue907'}</div>
               <div className='nodes__section'>
                 <div className='nodes__head'>
-                  <h3 className='nodes__header'>IDENTITY</h3>
-                  <Languages languages={{
-                    'rust': '/',
-                    'c': '/',
-                  }}/>
+                  <h3 className='nodes__header'>WASP</h3>
+                  <Languages languages={{}}/>
                 </div>
                 <ul className='nodes__features'>
-                  <li className='nodes__feature'>Identity framework</li>
-                  <li className='nodes__feature'>Decentralized Identifiers (DID)</li>
-                  <li className='nodes__feature'>Verifiable Credentials (VC)</li>
+                  <li className='nodes__feature'>Validate smart contracts</li>
+                  <li className='nodes__feature'>Run the IOTA Smart Contract Protocol</li>
+                  <li className='nodes__feature'>Deploy your IOTA Smart Contracts</li>
                 </ul>
                 <Link className='nodes__button button button--outline button--primary'>
-                  Create an identity
+                  Run a smart contract
                 </Link>
               </div>
             </div>
@@ -289,18 +298,18 @@ export default function HomeLayout() {
               <div className='nodes__icon'>{'\ue907'}</div>
               <div className='nodes__section'>
                 <div className='nodes__head'>
-                  <h3 className='nodes__header'>IDENTITY</h3>
+                  <h3 className='nodes__header'>BEE</h3>
                   <Languages languages={{
-                    'rust': '/',
+                    'Rust': '/',
                   }}/>
                 </div>
                 <ul className='nodes__features'>
-                  <li className='nodes__feature'>Identity framework</li>
-                  <li className='nodes__feature'>Decentralized Identifiers (DID)</li>
-                  <li className='nodes__feature'>Verifiable Credentials (VC)</li>
+                  <li className='nodes__feature'>Installing the Bee Node</li>
+                  <li className='nodes__feature'>Updating the Bee Node</li>
+                  <li className='nodes__feature'>Compiling the Bee Node</li>
                 </ul>
                 <Link className='nodes__button button button--outline button--primary'>
-                  Create an identity
+                  Set up a Bee node
                 </Link>
               </div>
             </div>
@@ -308,18 +317,18 @@ export default function HomeLayout() {
               <div className='nodes__icon'>{'\ue907'}</div>
               <div className='nodes__section'>
                 <div className='nodes__head'>
-                  <h3 className='nodes__header'>IDENTITY</h3>
+                  <h3 className='nodes__header'>HORNET</h3>
                   <Languages languages={{
-                    'rust': '/',
+                    'Go': '/',
                   }}/>
                 </div>
                 <ul className='nodes__features'>
-                  <li className='nodes__feature'>Identity framework</li>
-                  <li className='nodes__feature'>Decentralized Identifiers (DID)</li>
-                  <li className='nodes__feature'>Verifiable Credentials (VC)</li>
+                  <li className='nodes__feature'>Installing the Hornet node</li>
+                  <li className='nodes__feature'>Updating the Hornet node</li>
+                  <li className='nodes__feature'>Compiling the Hornet Node</li>
                 </ul>
                 <Link className='nodes__button button button--outline button--primary'>
-                  Create an identity
+                  Set up a Hornet node
                 </Link>
               </div>
             </div>
@@ -327,18 +336,20 @@ export default function HomeLayout() {
               <div className='nodes__icon'>{'\ue907'}</div>
               <div className='nodes__section'>
                 <div className='nodes__head'>
-                  <h3 className='nodes__header'>IDENTITY</h3>
+                  <h3 className='nodes__header'>CHRONICLE</h3>
                   <Languages languages={{
-                    'rust': '/',
+                    'Rust': '/',
+                    'WASM': '/',
+                    'C': '/',
                   }}/>
                 </div>
                 <ul className='nodes__features'>
-                  <li className='nodes__feature'>Identity framework</li>
-                  <li className='nodes__feature'>Decentralized Identifiers (DID)</li>
-                  <li className='nodes__feature'>Verifiable Credentials (VC)</li>
+                  <li className='nodes__feature'>Efficient and reliable permanodes</li>
+                  <li className='nodes__feature'>Solution for storing all transactions</li>
+                  <li className='nodes__feature'>Managing and accessing permanode</li>
                 </ul>
                 <Link className='nodes__button button button--outline button--primary'>
-                  Create an identity
+                  Try the permanode framework
                 </Link>
               </div>
             </div>
@@ -356,9 +367,9 @@ export default function HomeLayout() {
             </div>
             <div className='resources__card'>
               <div className='resources__icon'>{'\ue907'}</div>
-              <h3 className='resources__header'>Try a tutorial</h3>
+              <h3 className='resources__header'>Discover integrations & tools</h3>
               <p className='resources__description'>
-                Now that you understand the basics of IOTA.
+                Speed up integration with IOTA tools & solutions
               </p>
             </div>
           </div>
@@ -368,22 +379,22 @@ export default function HomeLayout() {
             <Link className='further__card'>
               <div className='further__section'>
                 <div className='further__icon'>{'\ue907'}</div>
-                <h3 className='further__header'>What is IOTA?</h3>
-                <p className='further__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit, pharetra proin quis egestas.</p>
+                <h3 className='further__header'>Join Discord</h3>
+                <p className='further__description'>Check out the IOTA community on Discord.</p>
               </div>
             </Link>
             <Link className='further__card'>
               <div className='further__section'>
                 <div className='further__icon'>{'\ue907'}</div>
-                <h3 className='further__header'>Get IOTA tokens</h3>
-                <p className='further__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h3 className='further__header'>Tangle Improvement Proposals</h3>
+                <p className='further__description'>Propose, discuss and debate ideas.</p>
               </div>
             </Link>
             <Link className='further__card'>
               <div className='further__section'>
                 <div className='further__icon'>{'\ue90a'}</div>
-                <h3 className='further__header'>Pick a wallet</h3>
-                <p className='further__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h3 className='further__header'>Visit the explorer</h3>
+                <p className='further__description'>View transactions and data stored on the IOTA Tangle.</p>
               </div>
             </Link>
           </div>
