04a108dfcfbc135e1d6758f69b3ae0f27547ff38
Dr-Electron <dr-electr0n@protonmail.com>
2022-07-12 15:26:58 +0000

    Restructure docs

---
+++ content/research/sidebars.ts
@@ -0,0 +1,16 @@
+module.exports = {
+    nodes: [
+        'research-outline',
+        'research-papers',
+        {
+            type: 'link',
+            label: 'Coordicide Specs',
+            href: 'https://stardust.iota-community.org/IOTA-2.0-Research-Specifications/Preface',
+        },
+        {
+            type: 'link',
+            label: 'GoShimmer',
+            href: 'https://stardust.iota-community.org/goshimmer/welcome',
+        }
+    ]
+};
