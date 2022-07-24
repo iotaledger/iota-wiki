150371685de1ca349f15034a56f06a8be0ae0a03
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-07-24 23:56:08 +0200

    Make libraries a category with custom page

---
+++ internal/categories/sidebars.ts
@@ -0,0 +1,46 @@
+module.exports = {
+  categories: [
+    {
+      type: 'category',
+      link: {
+        type: 'doc',
+        id: 'libraries',
+        
+      },
+      label: 'Libraries',
+      items: [
+        {
+          type: 'link',
+          label: 'Identity',
+          href: 'identity.rs/introduction',
+        },
+        {
+          type: 'link',
+          label: 'Client',
+          href: 'iota.rs/welcome',
+        },
+        {
+          type: 'link',
+          label: 'JS Client',
+          href: 'iotajs/welcome',
+        },
+        {
+          type: 'link',
+          label: 'Wallet',
+          href: 'wallet.rs/welcome',
+        },
+        {
+          type: 'link',
+          label: 'Streams',
+          href: 'streams/welcome',
+        },
+        {
+          type: 'link',
+          label: 'Stronghold',
+          href: 'stronghold.rs/getting_started',
+        },
+      ],
+      collapsible: false,
+    }
+  ]
+};
