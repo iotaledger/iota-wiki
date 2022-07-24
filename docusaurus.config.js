991c49367fba380e10ffd77c7ab8a2edefa1d5ec
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-07-24 23:56:08 +0200

    Make libraries a category with custom page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,8 +92,8 @@ module.exports = {
         },
         {
           label: 'Libraries',
-          to: 'libraries',
-          activeBaseRegex: '^/libraries.*',
+          to: 'categories/libraries',
+          activeBaseRegex: '^(/categories)?/libraries.*',
         },
         {
           label: 'Nodes',
@@ -337,6 +337,19 @@ module.exports = {
     'docusaurus-plugin-matomo',
     'docusaurus-plugin-hotjar',
     '@docusaurus/plugin-ideal-image',
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'categories',
+        path: 'internal/categories',
+        routeBasePath: 'categories',
+        sidebarPath: require.resolve('./internal/categories/sidebars.ts'),
+
+        // General config
+        editUrl: 'https://github.com/iota-community/iota-wiki/edit/main/',
+        showLastUpdateTime: true,
+      },
+    ],
     [
       '@docusaurus/plugin-content-docs',
       {
