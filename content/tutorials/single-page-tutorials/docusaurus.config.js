4573a71741cfb05f7897dcb0edf5e3b113c43b4a
huhn511 <sehe89@gmail.com>
2022-09-08 16:36:55 +0200

    Add tutorial 'Send Shimmer Tokens with Javascript' (#701)

--- content/tutorials/single-page-tutorials/docusaurus.config.js
+++ content/tutorials/single-page-tutorials/docusaurus.config.js
@@ -168,6 +168,17 @@ module.exports = {
                 tags: ['text', 'getting-started', 'node_js', 'wallet', 'ipfs', 'shimmer'],
             },
         ],
+        [
+            '@iota-wiki/plugin-tutorial',
+            {
+                title: 'Send Shimmer Tokens with Javascript (client lib)',
+                description:
+                    'In this tutorial you will learn how to send Shimmer Tokens in the testnet.',
+                preview: 'send-shimmer-tokens-with-javascript.png',
+                route: 'send-shimmer-tokens-with-javascript',
+                tags: ['text', 'shimmer', 'javascript', 'client', 'getting-started', 'iota.rs (js binding)', 'testnet'],
+            },
+        ],
     ],
     staticDirectories: [path.resolve(__dirname, './images')],
 };
