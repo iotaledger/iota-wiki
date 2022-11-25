3c6e7aca62955c252209c4dbce2140bf49a42ad8
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-11-25 12:01:32 +0100

    Refactor configs

---
+++ common/community/contribute-to-wiki/explanations/tools/writing_tools.md
@@ -0,0 +1,85 @@
+---
+description: You can use the following tools to help you write.
+image: /img/iota-wiki.png
+tags:
+  - write
+  - edit
+  - update
+  - grammar
+  - style
+  - technical writing
+  - blog
+  - tools
+---
+
+# Writing Tools
+
+There are several writing tools you can use to improve your [style and grammar](#style-and-grammar)
+, [structure](#structure), add [diagrams](../../reference/structure/overview)
+and learn about [markdown and Docusaurus components](#markdown-and-components).
+
+## Style and Grammar
+
+### Grammarly
+
+[Grammarly](https://www.grammarly.com/) is browser extension that checks your spelling, grammar, punctuation, style and
+even writing voice and tone.
+
+### Hemingwayapp
+
+[Hemingwayapp](https://hemingwayapp.com/) highlights and corrects grammar, fluency, and sentence structure in order
+to help your writing read and look better.
+
+### IOTA Wiki Style Guide
+
+This wiki has its own [style guide](../../reference/style/manifesto) you can use so your writing is cohesive with the
+rest of the wiki.
+
+## Markdown and Components
+
+### Markdown Guide
+
+This wiki uses [Markdown](https://www.markdownguide.org) as primary language. You can use
+the [cheat sheet](https://www.markdownguide.org/cheat-sheet/) to get started in no time.
+
+### Docusaurus
+
+This wiki is built using [Docusarus](https://docusaurus.io/). You can use
+their [documentation](https://docusaurus.io/docs) to learn all about its many features.
+
+## Diagrams
+
+### Diagrams.net
+
+[diagrams.net (formerly draw.io)](https://app.diagrams.net/) is free online diagram software. You can use it as a
+flowchart maker, network diagram software, to create UMLs online.
+
+### Design Resources
+
+You can find IOTA Wiki themed graphic resources in
+the [Design Resources repository](https://github.com/iota-wiki/Design-Resources). You can add this repository
+to [diagrams.net](#diagramsnet) in three simple steps:
+
+1. Open [diagrams.net](https://app.diagrams.net/).
+2. Click **File** > **Open Library From** > **URL**.
+3. Paste the RAW URL of the library, for example:
+
+```plaintext
+https://raw.githubusercontent.com/iota-wiki/Design-Resources/main/flowchart_library/flowchart.xml
+```
+
+## Structure
+
+### Diataxis
+
+Separating concerns in documentation makes it easier for the user to find what they are looking for. This wiki
+uses [Diataxis](https://diataxis.fr) to organize its documentation into four distinct categories. You can read more
+about Diataxis in the Wiki in the [software documentation structure article](../../reference/structure/overview.md).
+
+### IOTA Wiki
+
+### Templates
+
+The IOTA wiki hosts templates you can use
+to [start your project's documentation](https://github.com/iota-wiki/docs-template)
+and [write a tutorial](https://github.com/iota-wiki/tutorial-template/blob/main/documentation/docs/template.md)
---
+++ tutorials/docusaurus.config.js
@@ -0,0 +1,204 @@
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    [
+      '@docusaurus/plugin-content-pages',
+      {
+        id: 'single-page-tutorials',
+        path: path.resolve(__dirname, 'pages'),
+        routeBasePath: '/tutorials',
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Send IOTA Tokens with Javascript',
+        description:
+          'In this tutorial you will learn how to send IOTA Tokens in the Devnet.',
+        preview: 'send-iota-tokens-with-javascript.svg',
+        route: 'tutorials/send-iota-tokens-with-javascript',
+        tags: ['text', 'getting-started', 'client', 'js', 'mainnet'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Run a Wasp Node',
+        description: 'In this tutorial you will learn how to run a wasp node.',
+        preview: 'IOTA-Smart-Contract-Tutorials-A.jpg',
+        route: 'smart-contracts/guide/chains_and_nodes/running-a-node',
+        tags: ['text', 'video'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Run a Wasp Chain',
+        description: 'In this tutorial you will learn how to run a wasp chain.',
+        preview: 'IOTA-Smart-Contract-Tutorials-B.jpg',
+        route: 'smart-contracts/guide/chains_and_nodes/setting-up-a-chain',
+        tags: ['text', 'video'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Deploy a WASM Smart Contract',
+        description:
+          'In this tutorial you will learn how to deploy a wasm smart contract.',
+        preview: 'IOTA-Smart-Contract-Tutorials-C.jpg',
+        route:
+          'smart-contracts/guide/chains_and_nodes/setting-up-a-chain#deploying-a-wasm-contract',
+        tags: ['text', 'video'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Run a EVM Chain',
+        description: 'In this tutorial you will learn how to run an EVM node.',
+        preview: 'IOTA-Smart-Contract-Tutorials-D.jpg',
+        route: 'smart-contracts/guide/evm/create-chain',
+        tags: ['text', 'video'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Deploy Solidity Smart Contract using Remix + Metamask',
+        description:
+          'In this tutorial you will learn how to deploy a solidity smart contract on EVM using remix and metamask.',
+        preview: 'IOTA-Smart-Contract-Tutorials-E.jpg',
+        route: 'smart-contracts/guide/evm/tooling#remix',
+        tags: ['text', 'video'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Deploy Solidity Smart Contract using Hardhat',
+        description:
+          'In this tutorial you will learn how to deploy a solidity smart contract on EVM using hardhat.',
+        preview: 'IOTA-Smart-Contract-Tutorials-F.jpg',
+        route: 'smart-contracts/guide/evm/tooling#hardhat',
+        tags: ['text', 'video'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Generate WASM Smart Contract using Schema Tool',
+        description:
+          'In this tutorial you will learn how to use schema tool to generate smart contract template code on rust and golang',
+        preview: 'IOTA-Smart-Contract-Tutorials-G.jpg',
+        route: 'smart-contracts/guide/wasm_vm/usage',
+        tags: ['text', 'video'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Generate WASM Smart Contract using Schema Tool in Typescript',
+        description:
+          'In this tutorial you will learn how to use schema tool to generate smart contract template code on typescript',
+        preview: 'IOTA-Smart-Contract-Tutorials-G.jpg',
+        route: 'https://www.youtube.com/watch?v=P5HMgmY3DMM',
+        tags: ['video'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Send Shimmer Tokens with wallet.rs',
+        description:
+          'In this tutorial you will learn how to send Shimmer Tokens with wallet.rs.',
+        preview: 'send-shimmer-tokens-with-wallet-lib.png',
+        route: 'tutorials/send-shimmer-tokens-with-wallet-lib',
+        tags: ['text', 'wallet', 'getting-started', 'rust', 'shimmer'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Interact with IOTA Node Extension (INX)',
+        description:
+          'In this tutorial you will learn how to interact with the IOTA Node Extension (INX) interface based on gRPC.',
+        preview: 'hornet-inx-interaction.png',
+        route: 'tutorials/hornet-inx-interaction',
+        tags: ['text', 'getting-started', 'shimmer', 'js'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Tokenless Data Notarization',
+        description:
+          'Proove that a block was part of the past Tangle, even if this part of the Tangle was already pruned from network nodes.',
+        preview: 'proof-inclusion-of-a-block.png',
+        route: 'tutorials/proof-inclusion-of-a-block',
+        tags: ['text', 'getting-started', 'js', 'client', 'shimmer'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Setup testnet address and receive funds from faucet',
+        description:
+          'In this tutorial you will learn how to create a seed and address with wallet.rs and receive testnet tokens from the faucet.',
+        preview: 'wallet-setup.png',
+        route: 'tutorials/wallet-setup',
+        tags: ['text', 'getting-started', 'node_js', 'wallet', 'shimmer'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'How to Setup iota.js for Development',
+        description:
+          'In this tutorial you will learn how to create a new project, generate a new address and receive x funds from the faucet.',
+        preview: 'setup_iota-js_for_development.png',
+        route: 'tutorials/setup_iota-js_for_development',
+        tags: ['text', 'getting-started', 'client', 'shimmer', 'js'],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Create a NFT with wallet.rs and IPFS',
+        description:
+          'In this tutorial you will learn how to create a NFT on the Shimmer network with wallet.rs and IPFS.',
+        preview: 'create-nft-with-wallet-lib.png',
+        route: 'tutorials/create-nft-with-wallet-lib',
+        tags: [
+          'text',
+          'getting-started',
+          'node_js',
+          'wallet',
+          'ipfs',
+          'shimmer',
+        ],
+      },
+    ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Send Shimmer Tokens with Javascript',
+        description:
+          'In this tutorial you will learn how to send Shimmer Tokens in the testnet.',
+        preview: 'send-shimmer-tokens-with-javascript.png',
+        route: 'tutorials/send-shimmer-tokens-with-javascript',
+        tags: [
+          'text',
+          'shimmer',
+          'javascript',
+          'client',
+          'getting-started',
+          'iota.rs (js binding)',
+          'shimmer',
+        ],
+      },
+    ],
+  ],
+  staticDirectories: [path.resolve(__dirname, 'static')],
+};
