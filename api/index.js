4bf7bf8e46783b7f7de1cde92666e5412e4a4bab
marc2332 <mespinsanz@gmail.com>
2022-11-28 12:23:34 +0100

    fix: Serve 404

--- api/index.js
+++ api/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const { renderFile } = require('ejs');
 
 const PORT = process.env.PORT ?? 3000;
+const DOCS_BUILD = __dirname + './../build';
+
 const app = express();
 
 app.engine('html', renderFile);
 app.use(express.static('build', { extensions: ['html'] }));
+
+app.use(function (req, res) {
+  res.render(DOCS_BUILD + '/404.html');
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
