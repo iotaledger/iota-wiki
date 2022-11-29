556a13e114d7a57a8c5947fc13d4b9880e58ee36
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-11-29 04:04:58 +0100

    Use as much config from Next as possible

--- cli/src/docusaurus/config/build.js
+++ cli/src/docusaurus/config/build.js
@@ -1,68 +1,26 @@
 import path from 'path';
+import { merge } from '@wiki/utils/config';
+import common from './common';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const config = require(path.resolve(
+const { IOTA_WIKI_DIRECTORY } = process.env;
+
+const external = require(path.resolve(
   process.cwd(),
-  process.env.IOTA_WIKI_DIRECTORY,
-  './docusaurus.config.js',
+  IOTA_WIKI_DIRECTORY,
+  'docusaurus.config.js',
 ));
 
-module.exports = {
-  title: 'IOTA Wiki',
-  tagline: 'The complete reference for IOTA',
-  url: 'https://wiki.iota.org',
-  baseUrl: '/',
+const internal = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
   onDuplicateRoutes: 'throw',
-  favicon: 'img/favicon.ico',
-  trailingSlash: false,
-  organizationName: 'iota-community', // Usually your GitHub org/user name.
-  projectName: 'iota-wiki', // Usually your repo name.
-  stylesheets: [
-    'https://fonts.googleapis.com/css?family=Material+Icons',
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
-  ],
-  plugins: ['plugin-image-zoom', ...config.plugins],
-  themes: [
-    [
-      '@docusaurus/theme-classic',
-      {
-        customCss: path.resolve(__dirname, '../css/custom.css'),
-      },
-    ],
-    path.resolve(__dirname, '../theme'),
-  ],
   themeConfig: {
-    image: 'img/iota-wiki.png',
-    imageZoom: {
-      selector:
-        '.markdown :not(a) > img:not(.image-gallery-image):not(.image-gallery-thumbnail-image)',
-      // Optional medium-zoom options
-      // see: https://www.npmjs.com/package/medium-zoom#options
-      options: {
-        background: 'rgba(0, 0, 0, 0.6)',
-      },
-    },
     navbar: {
-      hideOnScroll: true,
       logo: {
-        alt: 'IOTA Wiki Logo',
-        src: 'img/logo.svg',
-        srcDark: 'img/logo_dark.svg',
         href: 'https://wiki.iota.org',
       },
-      items: [],
     },
   },
-  staticDirectories: [
-    path.resolve(__dirname, '../static'),
-    ...config.staticDirectories,
-  ],
 };
+
+module.exports = merge(external, common, internal);
--- cli/src/docusaurus/config/start.js
+++ cli/src/docusaurus/config/start.js
@@ -1,44 +1,19 @@
 import path from 'path';
+import { merge } from '@wiki/utils/config';
+import common from './common';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const config = require(path.resolve(
+const { IOTA_WIKI_DIRECTORY } = process.env;
+
+const external = require(path.resolve(
   process.cwd(),
-  process.env.IOTA_WIKI_DIRECTORY,
-  './docusaurus.config.js',
+  IOTA_WIKI_DIRECTORY,
+  'docusaurus.config.js',
 ));
 
-module.exports = {
-  title: 'IOTA Wiki',
-  tagline: 'The complete reference for IOTA',
-  url: 'https://wiki.iota.org',
-  baseUrl: '/',
+const internal = {
   onBrokenLinks: 'log',
   onBrokenMarkdownLinks: 'log',
   onDuplicateRoutes: 'log',
-  favicon: 'img/favicon.ico',
-  trailingSlash: false,
-  organizationName: 'iota-community', // Usually your GitHub org/user name.
-  projectName: 'iota-wiki', // Usually your repo name.
-  stylesheets: [
-    'https://fonts.googleapis.com/css?family=Material+Icons',
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
-  ],
-  plugins: ['plugin-image-zoom', ...config.plugins],
-  themes: [
-    [
-      '@docusaurus/theme-classic',
-      {
-        customCss: path.resolve(__dirname, '../css/custom.css'),
-      },
-    ],
-    path.resolve(__dirname, '../theme'),
-  ],
   themeConfig: {
     announcementBar: {
       id: 'preview',
@@ -46,40 +21,7 @@ module.exports = {
         'This is a local preview. Please find available routes <a href="/404">here</a>.',
       isCloseable: false,
     },
-    image: 'img/iota-wiki.png',
-    imageZoom: {
-      selector:
-        '.markdown :not(a) > img:not(.image-gallery-image):not(.image-gallery-thumbnail-image)',
-      // Optional medium-zoom options
-      // see: https://www.npmjs.com/package/medium-zoom#options
-      options: {
-        background: 'rgba(0, 0, 0, 0.6)',
-      },
-    },
-    navbar: {
-      hideOnScroll: true,
-      logo: {
-        alt: 'IOTA Wiki Logo',
-        src: 'img/logo.svg',
-        srcDark: 'img/logo_dark.svg',
-      },
-      items: [],
-    },
-    footer: {
-      links: [],
-      logo: {
-        alt: 'IOTA Logo',
-        src: 'img/iota_logo.svg',
-        href: 'https://www.iota.org',
-      },
-      copyright: `© ${new Date().getFullYear()} IOTA Wiki. Built with Docusaurus.`,
-    },
-    colorMode: {
-      defaultMode: 'dark',
-    },
   },
-  staticDirectories: [
-    path.resolve(__dirname, '../static'),
-    ...config.staticDirectories,
-  ],
 };
+
+module.exports = merge(external, common, internal);
