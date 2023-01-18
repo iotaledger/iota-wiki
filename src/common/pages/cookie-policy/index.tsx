10c05b4fbe7b7d1cb0673977840fc8fdfee3b584
Begoña Alvarez <balvarez@boxfish.studio>
2023-01-18 13:46:55 +0100

    feat: remove cookiebot duplicates

--- src/common/pages/cookie-policy/index.tsx
+++ src/common/pages/cookie-policy/index.tsx
@@ -1,9 +1,10 @@
 import Layout from '@theme/Layout';
-import React,{ useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 export default function CookiePolicy() {
   useEffect(() => {
-    const scriptPromise = new Promise((resolve, reject) => {
+
+    const appendCookiebotCdReport = new Promise((resolve, reject) => {
       const script = document.createElement('script');
       document.body.appendChild(script);
       script.onload = resolve;
@@ -13,21 +14,37 @@ export default function CookiePolicy() {
         'https://consent.cookiebot.com/8f051d60-4ecb-41a0-abb9-4874fd999e4f/cdreport.js?referer=iota-wiki-fawn.vercel.app';
     });
 
-    const script = document.createElement('script');
-    script.id = 'CookieDeclaration';
-    script.src =
-      'https://consent.cookiebot.com/8f051d60-4ecb-41a0-abb9-4874fd999e4f/cd.js';
-    script.type = 'text/javascript';
-    script.async = true;
-    const cookieDeclarationWrapper = document.getElementById(
-      'cookie-declaration-wrapper',
-    );
-    cookieDeclarationWrapper.appendChild(script);
-
-    scriptPromise.then(() => {
-      console.log('loaded stuff');
+    const appendCookiebotDeclaration = new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.id = 'CookieDeclaration';
+      script.src =
+        'https://consent.cookiebot.com/8f051d60-4ecb-41a0-abb9-4874fd999e4f/cd.js';
+      script.type = 'text/javascript';
+      script.async = true;
+      const cookieDeclarationWrapper = document.getElementById('cookie-declaration-wrapper');
+      document.getElementsByClassName('CookieDeclaration')
+      cookieDeclarationWrapper.appendChild(script);
+      script.onload = resolve;
+      script.onerror = reject;
     });
+
+    appendCookiebotCdReport.then(() => {
+      appendCookiebotDeclaration.then(() => {
+        const cookieDeclarations = document.getElementsByClassName('CookieDeclaration')
+        // Remove all duplicates
+        if (cookieDeclarations?.length > 1) {
+          for (let i = 1; i < cookieDeclarations.length; i++) {
+            cookieDeclarations[i].remove();
+          }
+        }
+      }).catch((error) => {
+        console.error(error)
+      });
+    }).catch((error) => {
+      console.error(error)
+    })
   }, []);
+
   return (
     <>
       <Layout>
