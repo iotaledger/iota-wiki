0d10ce3a90dbfc42455553beb8493aed49cc1585
Kumar Anirudha <mail@anirudha.dev>
2023-03-30 16:56:55 +0530

    updated config and image

--- tutorials/docusaurus.config.js
+++ tutorials/docusaurus.config.js
@@ -218,6 +218,26 @@ module.exports = {
         ],
       },
     ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title:
+          'Create a Simple Voting dApp on ShimmerEVM',
+        description:
+          'This tutorial will guide you as you write and deploy a voting dApp on ShimmerEVM. You can vote on a few options and view the results; all data will be stored on-chain.',
+        preview: '/shimmerevm-dapp-voting.jpg',
+        route: 'tutorials/shimmerevm-dapp-voting',
+        tags: [
+          'text',
+          'shimmer',
+          'shimmerevm',
+          'solidity',
+          'dapp',
+          'voting',
+          'isc',
+        ],
+      },
+    ],
   ],
   staticDirectories: [path.resolve(__dirname, 'static')],
 };
