3f35132302c47868329cf4fe053c1c30dadc0d50
Ani <5357586+anistark@users.noreply.github.com>
2023-03-31 18:33:19 +0530

    Add tutorial for deploying a Smart Contract using Hardhat (#908)

--- tutorials/docusaurus.config.js
+++ tutorials/docusaurus.config.js
@@ -218,6 +218,17 @@ module.exports = {
         ],
       },
     ],
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'Deploy a Solidity Smart Contract Using Hardhat',
+        description:
+          'In this tutorial, you will learn how to set up Hardhat and use it to build, test and deploy a simple smart contract on ShimmerEVM.',
+        preview: '/shimmerevm-hardhat.jpg',
+        route: 'tutorials/shimmerevm-hardhat',
+        tags: ['text', 'shimmer', 'solidity', 'shimmerevm', 'hardhat', 'iscp'],
+      },
+    ],
   ],
   staticDirectories: [path.resolve(__dirname, 'static')],
 };
