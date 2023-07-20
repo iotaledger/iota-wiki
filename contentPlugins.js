6e009722244821addd40fcf4bb66d3fb245c82c5
Dr-Electron <dr-electr0n@protonmail.com>
2023-07-20 14:46:27 +0200

    Cleanup configs

--- contentPlugins.js
+++ contentPlugins.js
@@ -1,19 +1,7 @@
 const path = require('path');
 
 module.exports = async () => {
-  return [
-    {
-      id: 'get-started',
-      path: path.resolve(__dirname, 'docs/get-started'),
-      sidebarPath: path.resolve(__dirname, 'docs/get-started/sidebars.ts'),
-      routeBasePath: 'get-started',
-    },
-    {
-      id: 'learn',
-      path: path.resolve(__dirname, 'docs/learn'),
-      sidebarPath: path.resolve(__dirname, 'docs/learn/sidebars.ts'),
-      routeBasePath: 'learn',
-    },
+  const buildPlugins = [
     {
       id: 'build',
       path: path.resolve(__dirname, 'docs/build/getting-started'),
@@ -31,8 +19,6 @@ module.exports = async () => {
       //overriding default exclude array to include the python api's classes with _ at the beginning
       //but still exclude any _admonitions
       exclude: [
-        // '**/_*.{js,jsx,ts,tsx,md}',
-        // '**/_*/**',
         '**/*.test.{js,jsx,ts,tsx}',
         '**/__tests__/**',
         '**/_admonitions/_**',
@@ -110,20 +96,33 @@ module.exports = async () => {
     {
       id: 'iota-rs-1-4-0',
       path: path.resolve(__dirname, 'docs/build/iota.rs/1.4.0/docs'),
-      routeBasePath: 'iota.rs/1.4.0',
+      routeBasePath: 'iota.rs',
       sidebarPath: path.resolve(
         __dirname,
         'docs/build/iota.rs/1.4.0/sidebars.js',
       ),
+      versions: {
+        current: {
+          label: '1.4.0',
+          badge: true,
+        },
+      },
     },
     {
       id: 'iota-rs-2-0-1-rc-7',
       path: path.resolve(__dirname, 'docs/build/iota.rs/2.0.1-rc.7/docs'),
-      routeBasePath: 'iota.rs/2.0.1-rc.7',
+      routeBasePath: 'iota.rs',
       sidebarPath: path.resolve(
         __dirname,
         'docs/build/iota.rs/2.0.1-rc.7/sidebars.js',
       ),
+      versions: {
+        current: {
+          label: '2.0.1-rc.7',
+          path: '2.0.1-rc.7',
+          badge: true,
+        },
+      },
     },
     {
       id: 'iotajs',
@@ -137,20 +136,33 @@ module.exports = async () => {
     {
       id: 'wallet-rs-0-1-0',
       path: path.resolve(__dirname, 'docs/build/wallet.rs/0.1.0/docs'),
-      routeBasePath: 'wallet.rs/0.1.0',
+      routeBasePath: 'wallet.rs',
       sidebarPath: path.resolve(
         __dirname,
         'docs/build/wallet.rs/0.1.0/sidebars.js',
       ),
+      versions: {
+        current: {
+          label: '0.1.0',
+          badge: true,
+        },
+      },
     },
     {
       id: 'wallet-rs-1-0-0-rc-6',
       path: path.resolve(__dirname, 'docs/build/wallet.rs/1.0.0-rc.6/docs'),
-      routeBasePath: 'wallet.rs/1.0.0-rc.6',
+      routeBasePath: 'wallet.rs',
       sidebarPath: path.resolve(
         __dirname,
         'docs/build/wallet.rs/1.0.0-rc.6/sidebars.js',
       ),
+      versions: {
+        current: {
+          label: '1.0.0-rc.6',
+          path: '1.0.0-rc.6',
+          badge: true,
+        },
+      },
     },
     {
       id: 'stronghold-rs',
@@ -170,6 +182,9 @@ module.exports = async () => {
         'docs/build/streams/0.2.0/sidebars.js',
       ),
     },
+  ];
+
+  const maintainPlugins = [
     {
       id: 'hornet-1-2-4',
       path: path.resolve(__dirname, 'docs/maintain/hornet/1.2.4/docs'),
@@ -250,5 +265,22 @@ module.exports = async () => {
         'docs/maintain/goshimmer/0.9.8/sidebars.js',
       ),
     },
+  ];
+
+  return [
+    {
+      id: 'get-started',
+      path: path.resolve(__dirname, 'docs/get-started'),
+      sidebarPath: path.resolve(__dirname, 'docs/get-started/sidebars.ts'),
+      routeBasePath: 'get-started',
+    },
+    {
+      id: 'learn',
+      path: path.resolve(__dirname, 'docs/learn'),
+      sidebarPath: path.resolve(__dirname, 'docs/learn/sidebars.ts'),
+      routeBasePath: 'learn',
+    },
+    ...buildPlugins,
+    ...maintainPlugins,
   ];
 };
