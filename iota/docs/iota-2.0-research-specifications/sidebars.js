59c8e3d81849cc9a7e70023d5c85fb54b4ed9e2c
Dr-Electron <dr-electr0n@protonmail.com>
2023-08-04 00:43:06 +0200

    Format

--- iota/docs/iota-2.0-research-specifications/sidebars.js
+++ iota/docs/iota-2.0-research-specifications/sidebars.js
@@ -10,32 +10,31 @@
  */
 
 module.exports = {
-
-    // But you can create a sidebar manually
-    /**/
-    tutorialSidebar: [
-        'Preface',
-        '1.1_introduction',
-        '2.2_message_layout',
-        '2.3_standard_payloads_layout',
-        '2.4_data_flow',
-        '3.3_peer_discovery',
-        '3.4_neighbor_selection',
-        '3.5_manual_peering',
-        '4.1_the_tangle',
-        '4.2_timestamps',
-        '4.3_tip_selection_algorithm',
-        '4.4_solidification',
-        '4.5_rate_control',
-        '4.6_congestion_control',
-        '4.7_markers',
-        '5.1_utxo',
-        '5.2_ledger_state',
-        '5.3_mana',
-        '6.1_objects_of_consensus',
-        '6.2_opinion_setting',
-        '6.3_fast_probabilistic_consensus',
-        '6.4_finalization',
-        '6.5_drng',
-    ],
+  // But you can create a sidebar manually
+  /**/
+  tutorialSidebar: [
+    'Preface',
+    '1.1_introduction',
+    '2.2_message_layout',
+    '2.3_standard_payloads_layout',
+    '2.4_data_flow',
+    '3.3_peer_discovery',
+    '3.4_neighbor_selection',
+    '3.5_manual_peering',
+    '4.1_the_tangle',
+    '4.2_timestamps',
+    '4.3_tip_selection_algorithm',
+    '4.4_solidification',
+    '4.5_rate_control',
+    '4.6_congestion_control',
+    '4.7_markers',
+    '5.1_utxo',
+    '5.2_ledger_state',
+    '5.3_mana',
+    '6.1_objects_of_consensus',
+    '6.2_opinion_setting',
+    '6.3_fast_probabilistic_consensus',
+    '6.4_finalization',
+    '6.5_drng',
+  ],
 };
