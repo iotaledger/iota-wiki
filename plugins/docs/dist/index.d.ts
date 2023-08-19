1e4ffe5d1dca6ac7cd7d476fdcfbc08c6ca779b8
Kumar Anirudha <mail@anirudha.dev>
2023-08-19 16:09:00 +0530

    updated nodejs version requirement

--- plugins/docs/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { LoadContext, Plugin } from '@docusaurus/types';
-import { OptionValidationContext, PluginOptions } from './types';
-import { LoadedContent as DocsContentLoaded } from '@docusaurus/plugin-content-docs';
-export default function pluginDocs(context: LoadContext, options: PluginOptions): Promise<Plugin<DocsContentLoaded>>;
-export declare function validateOptions({ validate, options, }: OptionValidationContext): PluginOptions;
