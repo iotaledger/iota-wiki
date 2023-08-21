5d2d50bfec35f9f833698aef603f5ceec9a29245
Jeroen van den Hout <jlvandenhout@gmail.com>
2023-08-21 17:40:23 +0200

    Add banner to Identity

--- contentPlugins.js
+++ contentPlugins.js
@@ -35,6 +35,7 @@ module.exports = async () => {
       id: 'identity-rs-0-5',
       path: path.resolve(__dirname, 'docs/build/identity.rs/v0.5.0/docs'),
       routeBasePath: 'identity.rs',
+      bannerPath: path.resolve(__dirname, 'banners/identity-deprecated.mdx'),
       sidebarPath: path.resolve(
         __dirname,
         'docs/build/identity.rs/v0.5.0/sidebars.js',
@@ -51,6 +52,10 @@ module.exports = async () => {
       id: 'identity-rs-0-6',
       path: path.resolve(__dirname, 'docs/build/identity.rs/v0.6.0/docs'),
       routeBasePath: 'identity.rs',
+      bannerPath: path.resolve(
+        __dirname,
+        'banners/identity-stronghold-migration.mdx',
+      ),
       sidebarPath: path.resolve(
         __dirname,
         'docs/build/identity.rs/v0.6.0/sidebars.js',
