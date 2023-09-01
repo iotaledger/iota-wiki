873bada907660860e932b58ed97bdd9c5597d555
Jeroen van den Hout <jlvandenhout@gmail.com>
2023-09-01 13:51:06 +0200

    Remove script

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,10 +22,6 @@ module.exports = async () => {
 
   const isProduction = MODE === 'production';
 
-  const scripts = {
-    scripts: ['/js/search-environment-observer.js'],
-  };
-
   const themeConfig = {
     themeConfig: {
       image: 'img/iota-wiki.png',
@@ -244,7 +240,6 @@ module.exports = async () => {
     },
     themeConfig,
     isProduction ? production : {},
-    scripts,
     ...additionalPlugins,
   );
 };
