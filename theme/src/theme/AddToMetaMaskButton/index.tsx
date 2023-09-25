cfa5f8cff39c19f019f357eda5dbe35db846e19d
Jeroen van den Hout <jlvandenhout@gmail.com>
2023-09-25 15:04:12 +0200

    Format and lint (#1185)
    * Format and lint
    
    * Update README.md
    
    Co-authored-by: Dr-Electron <dr-electr0n@protonmail.com>
    
    * Formatception
    
    ---------
    
    Co-authored-by: Dr-Electron <dr-electr0n@protonmail.com>

--- theme/src/theme/AddToMetaMaskButton/index.tsx
+++ theme/src/theme/AddToMetaMaskButton/index.tsx
@@ -1,69 +1,67 @@
 import React from 'react';
-import type {MetaMaskInpageProvider} from '@metamask/providers';
+import type { MetaMaskInpageProvider } from '@metamask/providers';
 
 declare global {
-    interface Window {
-        ethereum?: MetaMaskInpageProvider;
-    }
+  interface Window {
+    ethereum?: MetaMaskInpageProvider;
+  }
 }
 
 export const EVMNetworks = {
-    "shimmerevm-testnet": {
-        chainId: '0x430',
-        chainName: 'ShimmerEVM Testnet',
-        nativeCurrency: {
-            name: 'Shimmer',
-            symbol: 'SMR',
-            decimals: 18, // Replace with the number of decimals of the native currency
-        },
-        rpcUrls: ['https://json-rpc.evm.testnet.shimmer.network'],
-        blockExplorerUrls: [
-            'https://explorer.evm.testnet.shimmer.network/',
-        ],
+  'shimmerevm-testnet': {
+    chainId: '0x430',
+    chainName: 'ShimmerEVM Testnet',
+    nativeCurrency: {
+      name: 'Shimmer',
+      symbol: 'SMR',
+      decimals: 18, // Replace with the number of decimals of the native currency
     },
-    "shimmerevm": {
-        chainId: '0x94',
-        chainName: 'ShimmerEVM',
-        nativeCurrency: {
-            name: 'Shimmer',
-            symbol: 'SMR',
-            decimals: 18, // Replace with the number of decimals of the native currency
-        },
-        rpcUrls: ['https://json-rpc.evm.shimmer.network'],
-        blockExplorerUrls: [
-            'https://explorer.evm.shimmer.network/',
-        ],
-    }
+    rpcUrls: ['https://json-rpc.evm.testnet.shimmer.network'],
+    blockExplorerUrls: ['https://explorer.evm.testnet.shimmer.network/'],
+  },
+  shimmerevm: {
+    chainId: '0x94',
+    chainName: 'ShimmerEVM',
+    nativeCurrency: {
+      name: 'Shimmer',
+      symbol: 'SMR',
+      decimals: 18, // Replace with the number of decimals of the native currency
+    },
+    rpcUrls: ['https://json-rpc.evm.shimmer.network'],
+    blockExplorerUrls: ['https://explorer.evm.shimmer.network/'],
+  },
 };
 
 interface MetaMaskButtonProps {
-    cfg: unknown;
+  cfg: unknown;
 }
 
 export function AddToMetaMaskButton(props: MetaMaskButtonProps) {
-    async function addNetwork() {
-        if (!window.ethereum) {
-            alert('MetaMask is not installed. Please install MetaMask and try again.');
-            return;
-        }
-
-        try {
-            await window.ethereum.request({
-                method: 'wallet_addEthereumChain',
-                params: [props.cfg],
-            });
-        } catch (error) {
-            console.error(error);
-            console.log('Error adding network: ' + error.message);
-        }
+  async function addNetwork() {
+    if (!window.ethereum) {
+      alert(
+        'MetaMask is not installed. Please install MetaMask and try again.',
+      );
+      return;
     }
 
-    return (
-        <button
-            className={`button button--primary button--md margin-bottom--md`}
-            onClick={() => addNetwork()}
-        >
-            Add to MetaMask
-        </button>
-    );
+    try {
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [props.cfg],
+      });
+    } catch (error) {
+      console.error(error);
+      console.log('Error adding network: ' + error.message);
+    }
+  }
+
+  return (
+    <button
+      className={`button button--primary button--md margin-bottom--md`}
+      onClick={() => addNetwork()}
+    >
+      Add to MetaMask
+    </button>
+  );
 }
