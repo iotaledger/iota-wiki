f4825295dc6ba627a05238a1ead3ddf81887f744
Lucas Tortora <lucas.tortora@iota.org>
2023-09-26 09:26:26 -0300

    move EVM to the top of the L2 switcher fix build regex

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,8 +57,8 @@ module.exports = async () => {
               '^(/[^/]+)?/wallet.rs/.*|' +
               '^(/[^/]+)?/stronghold.rs/.*|' +
               '^(/[^/]+)?/streams/.*|' +
-              '^(/[^/]+)?/wasp-cli/.*' +
-              '^(/[^/]+)?/wasp-wasm/.*' +
+              '^(/[^/]+)?/wasp-cli/.*|' +
+              '^(/[^/]+)?/wasp-wasm/.*|' +
               '^(/[^/]+)?/wasp-evm/.*',
           },
           {
@@ -162,38 +162,6 @@ module.exports = async () => {
       ],
       plugins: [
         ...contentPlugins,
-        [
-          '@docusaurus/plugin-content-docs',
-          {
-            id: 'iota-tips',
-            path: path.resolve(__dirname, './docs/external/tips'),
-            routeBasePath: 'tips',
-            editUrl: ({ docPath }) =>
-              `https://github.com/iotaledger/tips/edit/main/${docPath}`,
-            remarkPlugins: [require('remark-import-partial')],
-            include: ['tips/**/*.md', 'README.md'],
-
-            // Create own sidebar to flatten hierarchy and use own labeling
-            async sidebarItemsGenerator({
-              defaultSidebarItemsGenerator,
-              ...args
-            }) {
-              const items = await defaultSidebarItemsGenerator(args);
-
-              const result = items[1].items.map((item) => {
-                if (item.type === 'category') {
-                  if (item.link.type === 'doc') {
-                    // Get TIP number and append TIP name
-                    item.label = item.link.id.slice(-4) + '-' + item.label;
-                  }
-                }
-                return item;
-              });
-
-              return [items[0]].concat(result);
-            },
-          },
-        ],
         [
           'docusaurus-plugin-openapi-docs',
           {
