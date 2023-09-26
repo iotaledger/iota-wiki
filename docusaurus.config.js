145e694bbc25102db05d2161a1e44fe84a8ec403
Lucas Tortora <lucas.tortora@iota.org>
2023-09-26 09:28:47 -0300

    remove unwanted change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -163,6 +163,38 @@ module.exports = async () => {
       plugins: [
         ...contentPlugins,
         [
+          '@docusaurus/plugin-content-docs',
+          {
+            id: 'iota-tips',
+            path: path.resolve(__dirname, './docs/external/tips'),
+            routeBasePath: 'tips',
+            editUrl: ({ docPath }) =>
+              `https://github.com/iotaledger/tips/edit/main/${docPath}`,
+            remarkPlugins: [require('remark-import-partial')],
+            include: ['tips/**/*.md', 'README.md'],
+
+            // Create own sidebar to flatten hierarchy and use own labeling
+            async sidebarItemsGenerator({
+              defaultSidebarItemsGenerator,
+              ...args
+            }) {
+              const items = await defaultSidebarItemsGenerator(args);
+
+              const result = items[1].items.map((item) => {
+                if (item.type === 'category') {
+                  if (item.link.type === 'doc') {
+                    // Get TIP number and append TIP name
+                    item.label = item.link.id.slice(-4) + '-' + item.label;
+                  }
+                }
+                return item;
+              });
+
+              return [items[0]].concat(result);
+            },
+          },
+        ],
+        [
           'docusaurus-plugin-openapi-docs',
           {
             id: 'openapi',
