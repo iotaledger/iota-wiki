eeef8282c39201bddf45a3934b30038d77b80b13
Dr-Electron <dr-electr0n@protonmail.com>
2023-10-03 14:48:46 +0200

    Implement link checker (#1207)
    * Implement link checker
    
    * Refactor logic
    
    * Add GH action
    
    * Simplify setup for testing
    
    * Test new workflow and log to stderr
    
    * Fix bug and re-add everything
    
    * Conditional run
    
    * Print output to terminal too
    
    * Change output order
    
    * Rename workflow
    
    * Check all plugins again
    
    * Format
    
    * Fix lint errors
    
    ---------
    
    Co-authored-by: Jeroen van den Hout <jlvandenhout@gmail.com>

--- cli/src/commands/check.tsx
+++ cli/src/commands/check.tsx
@@ -1,5 +1,9 @@
 import { Command, Option } from 'clipanion';
 import path from 'path';
+import visit from 'unist-util-visit';
+import { Node } from 'unist';
+import checkUrl from 'link-check';
+import { parse } from 'node-html-parser';
 
 const buildConfig = require.resolve('../docusaurus/config/build.js');
 const SUPPORTED_PLUGINS = [
@@ -7,6 +11,23 @@ const SUPPORTED_PLUGINS = [
   '@docusaurus/plugin-content-pages',
   '@iota-wiki/plugin-docs',
 ];
+const SUPPORTED_PROTOCOLS = ['http:', 'https:'];
+
+interface LinkNode extends Node {
+  url: string;
+}
+
+interface JsxOrHtmlNode extends Node {
+  value: string;
+}
+
+function isLinkNode(node: Node): node is LinkNode {
+  return node.type === 'link';
+}
+
+function isJsxOrHtmlNode(node: Node): node is JsxOrHtmlNode {
+  return node.type === 'jsx' || node.type === 'html';
+}
 
 export class Check extends Command {
   static paths = [[`check`]];
@@ -20,6 +41,7 @@ export class Check extends Command {
   });
 
   async execute() {
+    console.time('check');
     const { engine } = await import('unified-engine');
     const { remark } = await import('remark');
 
@@ -43,21 +65,109 @@ export class Check extends Command {
       return pluginPaths;
     }, []);
 
-    return await new Promise<number>((resolve, reject) =>
+    const urls = [];
+
+    // A Remark plugin that visits all link-like elements, extracts the URLs,
+    // and adds them to the `urls` array.
+    function RemarkLinkVisitor() {
+      return async (tree) => {
+        visit(tree, (node) => {
+          if (isLinkNode(node)) urls.push(node.url);
+          if (isJsxOrHtmlNode(node)) {
+            const element = parse(node.value);
+            if (element.tagName === 'a')
+              urls.push(element.getAttribute('href'));
+          }
+        });
+        return tree;
+      };
+    }
+
+    // The Unified engine that runs the RemarkLinkVisitor plugin on all
+    // reachable markdown files.
+    await new Promise<number>((resolve, reject) =>
       engine(
         {
           processor: remark(),
-          files: pluginPaths,
+          files: [pluginPaths],
           extensions: ['md', 'mdx'],
-          plugins: ['remark-lint-no-dead-urls'],
-          color: true,
-          quiet: true,
-          frail: true,
+          plugins: [RemarkLinkVisitor],
+          silent: true,
         },
         (error, status) => {
           error ? reject(error) : resolve(status);
         },
       ),
     );
+    const results = [];
+
+    // Validate URLs, extract the remote URLs, and gather them by hostname.
+    const urlsByHostname = urls.reduce<Map<string, Set<string>>>(
+      (urlsByHostname, url) => {
+        let validUrl: URL;
+
+        try {
+          validUrl = new URL(url);
+        } catch (error) {
+          results.push({ status: 'error', url, message: error });
+          return urlsByHostname;
+        }
+
+        if (
+          validUrl.hostname !== 'localhost' &&
+          SUPPORTED_PROTOCOLS.includes(validUrl.protocol)
+        ) {
+          if (urlsByHostname.has(validUrl.hostname)) {
+            urlsByHostname.get(validUrl.hostname).add(validUrl.href);
+          } else {
+            urlsByHostname.set(validUrl.hostname, new Set([validUrl.href]));
+          }
+        } else {
+          results.push({ status: 'error', url, message: 'ignored' });
+        }
+
+        return urlsByHostname;
+      },
+      new Map<string, Set<string>>(),
+    );
+
+    // Check all links for liveness. The checking is done in sequence per hostname
+    // to prevent getting rate limitted on a hostname.
+    // It will build an array of objects with a status of `alive`, `dead`, or `error` and
+    // a message providing info about the result.
+    await Promise.all(
+      Array.from(urlsByHostname.values()).map(async (urls) => {
+        for (const url of Array.from(urls)) {
+          await new Promise<void>((resolve) => {
+            checkUrl(url, (error, result) => {
+              if (error) results.push({ status: 'error', url, message: error });
+              else
+                results.push({
+                  status: result.status,
+                  url,
+                  message: result.statusCode,
+                });
+              resolve();
+            });
+          });
+        }
+      }),
+    );
+
+    const segragatedResults = {
+      error: results.filter(({ status }) => status === 'error'),
+      alive: results.filter(({ status }) => status === 'alive'),
+      dead: results.filter(({ status }) => status === 'dead'),
+    };
+
+    console.error(`${JSON.stringify(segragatedResults.dead, null, 4)}`);
+
+    console.log(
+      `alive: ${segragatedResults.alive.length}, dead: ${segragatedResults.dead.length}, error: ${segragatedResults.error.length}`,
+    );
+
+    console.timeEnd('check');
+
+    return segragatedResults.dead.length > 0 ? 1 : 0;
   }
 }
