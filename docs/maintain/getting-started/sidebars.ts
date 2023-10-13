93389fd8e7e12a0acdfab4e18b1d17c4da494835
Dr-Electron <dr-electr0n@protonmail.com>
2023-10-14 01:18:11 +0200

    Move community INX plugins

--- docs/maintain/getting-started/sidebars.ts
+++ docs/maintain/getting-started/sidebars.ts
@@ -1,17 +1,3 @@
 module.exports = {
-  maintain: [
-    'welcome',
-    {
-      type: 'category',
-      label: 'Community INX Plugins',
-      collapsed: true,
-      items: [
-        {
-          label: 'INX Collector',
-          type: 'link',
-          href: 'https://github.com/teleconsys/inx-collector',
-        },
-      ],
-    },
-  ],
+  maintain: ['welcome'],
 };
