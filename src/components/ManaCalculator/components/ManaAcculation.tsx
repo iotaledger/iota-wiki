c3460e4e5d3baf739c41280e90285e7abaedc08a
marc2332 <mespinsanz@gmail.com>
2023-11-03 13:24:54 +0100

    prettify

--- src/components/ManaCalculator/components/ManaAcculation.tsx
+++ src/components/ManaCalculator/components/ManaAcculation.tsx
@@ -21,25 +21,21 @@ export function ManaAccumulation({ results }: { results: EpochReward[] }) {
           margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
         >
           <defs>
-            <linearGradient id='colorUv' x1='0' y1='0' x2='0' y2='1'>
+            <linearGradient id='manaColor' x1='0' y1='0' x2='0' y2='1'>
               <stop offset='5%' stopColor='#8884d8' stopOpacity={0.8} />
               <stop offset='95%' stopColor='#8884d8' stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id='colorPv' x1='0' y1='0' x2='0' y2='1'>
-              <stop offset='5%' stopColor='#82ca9d' stopOpacity={0.8} />
-              <stop offset='95%' stopColor='#82ca9d' stopOpacity={0} />
             </linearGradient>
           </defs>
           <XAxis dataKey='epoch' />
           <YAxis width={120} />
-          <CartesianGrid strokeDasharray='3 3' />
+          <CartesianGrid strokeDasharray='3 3' stroke='rgb(255, 255, 255, 0.15)' />
           <Tooltip />
           <Area
             type='monotone'
             dataKey='mana'
             stroke='#8884d8'
             fillOpacity={1}
-            fill='url(#colorUv)'
+            fill='url(#manaColor)'
           />
         </AreaChart>
       </ResponsiveContainer>
