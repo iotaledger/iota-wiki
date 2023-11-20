73156b8866377fa33a17b0b087f29b38ff50c265
Dr-Electron <dr-electr0n@protonmail.com>
2023-11-20 22:55:08 +0100

    Format

--- docs/build/iota-sandbox/sidebars.js
+++ docs/build/iota-sandbox/sidebars.js
@@ -1,34 +1,33 @@
-module.exports = {  
-    docs: [
+module.exports = {
+  docs: [
+    {
+      type: 'doc',
+      id: 'welcome',
+      label: 'Welcome',
+    },
+    {
+      type: 'category',
+      label: 'How to',
+      items: [
         {
-        type: 'doc',
-        id: 'welcome',
-        label: 'Welcome',
+          type: 'doc',
+          id: 'how-tos/install',
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'References',
+      items: [
+        {
+          type: 'doc',
+          id: 'references/endpoints',
         },
         {
-            type: 'category',
-            label: 'How to',
-            items: [
-                {
-                    type: 'doc',
-                    id: 'how-tos/install',
-                }
-            ]
+          type: 'doc',
+          id: 'references/keys',
         },
-        {
-            type: 'category',
-            label: 'References',
-            items: [
-                {
-                    type: 'doc',
-                    id: 'references/endpoints',
-                },
-                {
-                    type: 'doc',
-                    id: 'references/keys',
-                }
-            ]
-        },
-    ],
+      ],
+    },
+  ],
 };
-  
