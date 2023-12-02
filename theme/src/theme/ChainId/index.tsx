074b05603058c2645a59f7fc4771714c35eafe06
Dr-Electron <dr-electr0n@protonmail.com>
2023-12-02 18:10:54 +0100

    Automatically pull chain id

---
+++ theme/src/theme/ChainId/index.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import { Web3 } from 'web3';
+
+interface ChainIdProps {
+  url: string;
+}
+
+export function ChainId(props: ChainIdProps) {
+  const [value, setValue] = useState<string | null>(null);
+
+  useEffect(() => {
+    const web3 = new Web3(props.url);
+    web3.eth
+      .getChainId()
+      .then((chainId) => setValue(chainId.toString()))
+      .catch((error) => setValue(`Error: ${error.message}`));
+  }, []);
+
+  return value;
+}
