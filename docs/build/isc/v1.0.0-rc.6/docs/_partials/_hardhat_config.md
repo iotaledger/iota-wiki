c95a81a03c0a65848a52364acedb519477333b9c
Dr-Electron <dr-electr0n@protonmail.com>
2024-04-28 16:07:23 +0200

    Update network info

---
+++ docs/build/isc/v1.0.0-rc.6/docs/_partials/_hardhat_config.md
@@ -0,0 +1,52 @@
+import CodeBlock from '@theme/CodeBlock';
+import { Networks } from '@theme/constant';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+
+<Tabs groupId='network'>
+<TabItem value='shimmer_testnet' label='ShimmerEVM Testnet'>
+
+<CodeBlock language="js">
+{`
+networks: {
+    'shimmerevm-testnet': {
+        url: '${Networks['shimmer_testnet'].evm.rpcUrls[0]}',
+        chainId: ${parseInt(Networks['shimmer_testnet'].evm.chainId)},
+        accounts: [YOUR PRIVATE KEY],
+    },
+}
+`}
+</CodeBlock>
+
+</TabItem>
+<TabItem value='iota_testnet' label='IotaEVM Testnet'>
+
+<CodeBlock language="js">
+{`
+networks: {
+    'iotaevm-testnet': {
+        url: '${Networks['iota_testnet'].evm.rpcUrls[0]}',
+        chainId: ${parseInt(Networks['iota_testnet'].evm.chainId)},
+        accounts: [YOUR PRIVATE KEY],
+    },
+}
+`}
+</CodeBlock>
+
+</TabItem>
+<TabItem value='shimmer' label='ShimmerEVM'>
+
+<CodeBlock language="js">
+{`
+networks: {
+    'shimmerevm': {
+        url: '${Networks['shimmer'].evm.rpcUrls[0]}',
+        chainId: ${parseInt(Networks['shimmer'].evm.chainId)},
+        accounts: [YOUR PRIVATE KEY],
+    },
+}
+`}
+</CodeBlock>
+
+</TabItem>
+</Tabs>
--- theme/src/theme/constant.tsx
+++ theme/src/theme/constant.tsx
@@ -1,28 +1,39 @@
 export const Networks = {
-  testnet: {
+  iota: {
+    baseToken: 'IOTA Token',
+    protocol: 'Stardust',
+    httpRestApi: 'https://api.stardust-mainnet.iotaledger.net',
+    eventApi: 'wss://api.stardust-mainnet.iotaledger.net:443 (MQTT 3.1, /mqtt)',
+    permaNodeApi: 'https://chronicle.stardust-mainnet.iotaledger.net',
+    explorer: 'https://explorer.iota.org/mainnet',
+  },
+  iota_testnet: {
     baseToken: 'Testnet Token (no value)',
     protocol: 'Stardust',
-    httpRestApi: 'https://api.testnet.shimmer.network',
-    eventApi: 'wss://api.testnet.shimmer.network:443/api/mqtt/v1 (MQTT 3.1)',
-    permaNodeApi: 'https://chronicle.testnet.shimmer.network',
-    faucet: 'https://faucet.testnet.shimmer.network',
-    explorer: 'https://explorer.shimmer.network/shimmer-testnet',
+    httpRestApi: 'https://api.testnet.iotaledger.net',
+    eventApi: 'wss://api.testnet.iotaledger.net:443 (MQTT 3.1, /mqtt)',
+    permaNodeApi: 'https://chronicle.testnet.iotaledger.net',
+    faucet: 'https://faucet.testnet.iotaledger.net',
+    explorer: 'https://explorer.iota.org/iota-testnet',
     evm: {
-      chainId: '0x431',
-      chainName: 'EVM Testnet',
+      chainId: '0x433',
+      chainName: 'IotaEVM Testnet',
       nativeCurrency: {
-        name: 'Shimmer',
-        symbol: 'SMR',
-        decimals: 18, // Replace with the number of decimals of the native currency
+        name: 'IOTA',
+        symbol: 'IOTA',
+        decimals: 18,
       },
-      rpcUrls: ['https://json-rpc.evm.testnet.shimmer.network'],
-      blockExplorerUrls: ['https://explorer.evm.testnet.shimmer.network/'],
+      rpcUrls: [
+        'https://json-rpc.evm.testnet.iotaledger.net',
+        'wss://ws.json-rpc.evm.testnet.iotaledger.net',
+      ],
+      blockExplorerUrls: ['https://explorer.evm.testnet.iotaledger.net'],
     },
     evmCustom: {
       chainAddress:
-        'rms1ppp00k5mmd2m8my8ukkp58nd3rskw6rx8l09aj35984k74uuc5u2cywn3ex',
+        'tst1pzxsrr7apqkdzz633dyntmvxwtyvk029p39te5j0m33q6946h7akzv663zu',
       aliasId:
-        '0x42f7da9bdb55b3ec87e5ac1a1e6d88e16768663fde5eca3429eb6f579cc538ac',
+        '0x8d018fdd082cd10b518b4935ed8672c8cb3d450c4abcd24fdc620d16babfbb61',
     },
   },
   shimmer: {
@@ -38,7 +49,7 @@ export const Networks = {
       nativeCurrency: {
         name: 'Shimmer',
         symbol: 'SMR',
-        decimals: 18, // Replace with the number of decimals of the native currency
+        decimals: 18,
       },
       rpcUrls: [
         'https://json-rpc.evm.shimmer.network',
@@ -53,32 +64,30 @@ export const Networks = {
         '0xccc7018e4fa63e5014332f45ddc8a5450da89572676d12d4d5e51c98d64155b3',
     },
   },
-  iota: {
-    baseToken: 'IOTA Token',
+  shimmer_testnet: {
+    baseToken: 'Testnet Token (no value)',
     protocol: 'Stardust',
-    httpRestApi: 'https://api.stardust-mainnet.iotaledger.net',
-    eventApi: 'wss://api.stardust-mainnet.iotaledger.net:443 (MQTT 3.1, /mqtt)',
-    permaNodeApi: 'https://chronicle.stardust-mainnet.iotaledger.net',
-    explorer: 'https://explorer.iota.org/mainnet',
+    httpRestApi: 'https://api.testnet.shimmer.network',
+    eventApi: 'wss://api.testnet.shimmer.network:443/api/mqtt/v1 (MQTT 3.1)',
+    permaNodeApi: 'https://chronicle.testnet.shimmer.network',
+    faucet: 'https://faucet.testnet.shimmer.network',
+    explorer: 'https://explorer.shimmer.network/shimmer-testnet',
     evm: {
-      chainId: '0x2276',
-      chainName: 'IotaEVM',
+      chainId: '0x431',
+      chainName: 'ShimmerEVM Testnet',
       nativeCurrency: {
-        name: 'IOTA',
-        symbol: 'IOTA',
-        decimals: 18, // Replace with the number of decimals of the native currency
+        name: 'Shimmer',
+        symbol: 'SMR',
+        decimals: 18,
       },
-      rpcUrls: [
-        'https://json-rpc.evm.iotaledger.net',
-        'wss://ws.json-rpc.evm.iotaledger.net',
-      ],
-      blockExplorerUrls: ['https://iota-evm.blockscout.com'],
+      rpcUrls: ['https://json-rpc.evm.testnet.shimmer.network'],
+      blockExplorerUrls: ['https://explorer.evm.testnet.shimmer.network/'],
     },
     evmCustom: {
       chainAddress:
-        'iota1pzt3mstq6khgc3tl0mwuzk3eqddkryqnpdxmk4nr25re2466uxwm28qqxu5',
+        'rms1ppp00k5mmd2m8my8ukkp58nd3rskw6rx8l09aj35984k74uuc5u2cywn3ex',
       aliasId:
-        '0x971dc160d5ae8c457f7eddc15a39035b6190130b4dbb5663550795575ae19db5',
+        '0x42f7da9bdb55b3ec87e5ac1a1e6d88e16768663fde5eca3429eb6f579cc538ac',
     },
   },
 };
