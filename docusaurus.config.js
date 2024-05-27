4d7f6652c28a66efa42a55068227a0ee5bab0aa0
Jeroen van den Hout <jlvandenhout@gmail.com>
2024-05-28 01:04:34 +0200

    Move theme components

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -444,7 +444,6 @@ module.exports = async () => {
       themes: [
         'docusaurus-theme-openapi-docs',
         '@saucelabs/theme-github-codeblock',
-        '@iota-wiki/theme',
       ],
       staticDirectories: [path.resolve(__dirname, 'static')],
     },
