a531cb437829e628f1bc9b20a8c306e840717c1b
Jeroen van den Hout <jlvandenhout@gmail.com>
2024-05-28 20:14:13 +0200

    Fix stylesheets

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -435,15 +435,6 @@ module.exports = async () => {
         ],
         'plugin-image-zoom',
       ],
-      stylesheets: [
-        {
-          href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-          type: 'text/css',
-          integrity:
-            'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-          crossorigin: 'anonymous',
-        },
-      ],
       themes: [
         'docusaurus-theme-openapi-docs',
         '@saucelabs/theme-github-codeblock',
