813dc76e2ef4437bc6a2df955684fe9c5513bccd
Dr-Electron <dr-electr0n@protonmail.com>
2024-05-31 22:54:10 +0200

    Fix smaller issues

--- scripts/get_wasp_references.sh
+++ scripts/get_wasp_references.sh
@@ -6,16 +6,16 @@ cd tmp
 
 # Download and copy docs
 curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.1/iscmagic.tar.gz | tar xzv
-cp -Rv docs/iscmagic/* ../docs/build/isc/v1.1-rc/docs/reference/magic-contract/
+cp -Rv docs/iscmagic/* ../docs/build/isc/v1.1/docs/reference/magic-contract/
 
 curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.1/iscutils.tar.gz | tar xzv
-cp -Rv docs/iscutils ../docs/build/isc/v1.1-rc/docs/reference/
+cp -Rv docs/iscutils ../docs/build/isc/v1.1/docs/reference/
 
 curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.3/iscmagic.tar.gz | tar xzv
-cp -Rv docs/iscmagic/* ../docs/build/isc/v1.2-alpha/docs/reference/magic-contract/
+cp -Rv docs/iscmagic/* ../docs/build/isc/v1.3-alpha/docs/reference/magic-contract/
 
 curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.3/iscutils.tar.gz | tar xzv
-cp -Rv docs/iscutils ../docs/build/isc/v1.2-alpha/docs/reference/
+cp -Rv docs/iscutils ../docs/build/isc/v1.3-alpha/docs/reference/
 
 # Return to root and cleanup
 cd -
