1b4fdf605f533222681309e780025470eba3c99a
Dr-Electron <dr-electr0n@protonmail.com>
2024-06-03 20:51:05 +0200

    Update sidebars

--- docs/build/isc/v1.3-alpha/sidebars.js
+++ docs/build/isc/v1.3-alpha/sidebars.js
@@ -86,6 +86,11 @@ module.exports = {
         },
         {
           type: 'doc',
+          label: 'Send ERC20 Tokens Across Chains',
+          id: 'how-tos/send-ERC20-across-chains',
+        },
+        {
+          type: 'doc',
           label: 'Create NFTs - ERC721',
           id: 'how-tos/ERC721',
         },
@@ -208,11 +213,6 @@ module.exports = {
               id: 'how-tos/core-contracts/call-view',
             },
           ],
-        },
-        {
-          type: 'doc',
-          label: 'Send ERC20 Tokens Across Chains',
-          id: 'how-tos/send-ERC20-across-chains',
         },
       ],
     },
