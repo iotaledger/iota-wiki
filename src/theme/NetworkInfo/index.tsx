2e52e1669f714430d30ce4cdf914cf8c6b43d72d
Lucas Tortora <lucas.tortora@iota.org>
2024-06-03 17:52:11 -0300

    test

--- src/theme/NetworkInfo/index.tsx
+++ src/theme/NetworkInfo/index.tsx
@@ -53,62 +53,84 @@ function Evm(props: NetworkProps) {
   return (
     <table>
       <tbody>
+      <tr>
+        <th>Base Token</th>
+        <td>{props.baseToken}</td>
+      </tr>
+      <tr>
+        <th>Protocol</th>
+        <td>ISC / EVM</td>
+      </tr>
+      <tr>
+        <th>Chain ID</th>
+        <td>
+          <ChainId url={props.evm.rpcUrls[0]} />
+        </td>
+      </tr>
+      <tr>
+        <th>RPC URL</th>
+        <td>
+          {props.evm.rpcUrls.map((url, index) => (
+            <CodeBlock key={index}>{url}</CodeBlock>
+          ))}
+        </td>
+      </tr>
+      {props.evmCustom.blastApiUrls && (
         <tr>
-          <th>Base Token</th>
-          <td>{props.baseToken}</td>
-        </tr>
-        <tr>
-          <th>Protocol</th>
-          <td>ISC / EVM</td>
-        </tr>
-        <tr>
-          <th>Chain ID</th>
+          <th>
+            <Admonition type="tip" title="Blast API URLs">
+              <a href={'/build/blastAPI/'}>Blast API</a> provides highly
+              scalable fault-tolerant API endpoints.
+            </Admonition>
+          </th>
           <td>
-            <ChainId url={props.evm.rpcUrls[0]} />
+            {props.evmCustom.blastApiUrls.map((object, index) =>
+              typeof object === 'string' ? (
+                <CodeBlock key={index}> {object as string} </CodeBlock>
+              ) : (
+                <CodeBlock title={Object.keys(object)[0]} key={index}>
+                  {' '}
+                  {Object.values(object)[0]}{' '}
+                </CodeBlock>
+              ),
+            )}
           </td>
         </tr>
-        <tr>
-          <th>RPC URL</th>
-          <td>
-            {props.evm.rpcUrls.map((url, index) => (
-              <CodeBlock key={index}>{url}</CodeBlock>
-            ))}
-          </td>
-        </tr>
-        {props.evm.blastApiUrls && (
-          <tr>
-            <th>
-              <Admonition type='tip' title='Blast API URLs'>
-                <a href={'/build/blastAPI/'}>Blast API</a> provides highly
-                scalable fault-tolerant API endpoints.
-              </Admonition>
-            </th>
-            <td>
-              {props.evm.blastApiUrls.map((object, index) =>
-                typeof object === 'string' ? (
-                  <CodeBlock key={index}> {object as string} </CodeBlock>
-                ) : (
-                  <CodeBlock title={Object.keys(object)[0]} key={index}>
-                    {' '}
-                    {Object.values(object)[0]}{' '}
-                  </CodeBlock>
-                ),
-              )}
-            </td>
-          </tr>
-        )}
-        <tr>
-          <th>Explorer</th>
-          <td>
-            <a
-              href={props.evm.blockExplorerUrls[0]}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {props.evm.blockExplorerUrls[0]}
-            </a>
-          </td>
-        </tr>
+      )}
+      <tr>
+        <th>Explorer</th>
+        <td>
+          <a
+            href={props.evm.blockExplorerUrls[0]}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {props.evm.blockExplorerUrls[0]}
+          </a>
+        </td>
+      </tr>
+      <tr>
+        <th>
+          {props.evmCustom.toolkit.hasFaucet
+            ? 'Toolkit & Faucet'
+            : 'Toolkit'}
+        </th>
+        <td>
+          <a
+            href={props.evmCustom.toolkit.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {props.evmCustom.toolkit.url}
+          </a>
+        </td>
+      </tr>
+      <tr>
+        <th>WASP API</th>
+        <td>
+          <CodeBlock> {props.evmCustom.api} </CodeBlock>
+        </td>
+      </tr>
       </tbody>
     </table>
   );
@@ -118,22 +140,22 @@ function EvmCustom(props: NetworkProps) {
   return (
     <table>
       <tbody>
-        <tr>
-          <th>Chain Address</th>
-          <td>
-            <a
-              href={props.explorer + '/addr/' + props.evmCustom.chainAddress}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {props.evmCustom.chainAddress}
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <th>Alias ID</th>
-          <td>{props.evmCustom.aliasId}</td>
-        </tr>
+      <tr>
+        <th>Chain Address</th>
+        <td>
+          <a
+            href={props.explorer + '/addr/' + props.evmCustom.chainAddress}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {props.evmCustom.chainAddress}
+          </a>
+        </td>
+      </tr>
+      <tr>
+        <th>Alias ID</th>
+        <td>{props.evmCustom.aliasId}</td>
+      </tr>
       </tbody>
     </table>
   );
