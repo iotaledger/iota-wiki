767c757a47dc1cfe91afc1073a06e9fa2564ec83
Dr-Electron <dr-electr0n@protonmail.com>
2024-06-03 19:40:54 +0200

    Restructure

--- src/theme/NetworkInfo/index.tsx
+++ src/theme/NetworkInfo/index.tsx
@@ -64,18 +64,18 @@ function Evm(props: NetworkProps) {
         <tr>
           <th>Chain ID</th>
           <td>
-            <ChainId url={props.evm.rpcUrls[0]} />
+            <ChainId url={props.evm.core.rpcUrls[0]} />
           </td>
         </tr>
         <tr>
           <th>RPC URL</th>
           <td>
-            {props.evm.rpcUrls.map((url, index) => (
+            {props.evm.core.rpcUrls.map((url, index) => (
               <CodeBlock key={index}>{url}</CodeBlock>
             ))}
           </td>
         </tr>
-        {props.evm.blastApiUrls && (
+        {props.evm.tools && props.evm.tools.blastApiUrls && (
           <tr>
             <th>
               <Admonition type='tip' title='Blast API URLs'>
@@ -84,7 +84,7 @@ function Evm(props: NetworkProps) {
               </Admonition>
             </th>
             <td>
-              {props.evm.blastApiUrls.map((object, index) =>
+              {props.evm.tools.blastApiUrls.map((object, index) =>
                 typeof object === 'string' ? (
                   <CodeBlock key={index}> {object as string} </CodeBlock>
                 ) : (
@@ -101,30 +101,30 @@ function Evm(props: NetworkProps) {
           <th>Explorer</th>
           <td>
             <a
-              href={props.evm.blockExplorerUrls[0]}
+              href={props.evm.core.blockExplorerUrls[0]}
               target='_blank'
               rel='noopener noreferrer'
             >
-              {props.evm.blockExplorerUrls[0]}
+              {props.evm.core.blockExplorerUrls[0]}
             </a>
           </td>
         </tr>
         <tr>
-          <th>{props.evm.toolkit.hasFaucet ? 'Toolkit & Faucet' : 'Toolkit'}</th>
+          <th>{props.evm.custom.toolkit.hasFaucet ? 'Toolkit & Faucet' : 'Toolkit'}</th>
           <td>
             <a
-              href={props.evm.toolkit.url}
+              href={props.evm.custom.toolkit.url}
               target='_blank'
               rel='noopener noreferrer'
             >
-              {props.evm.toolkit.url}
+              {props.evm.custom.toolkit.url}
             </a>
           </td>
         </tr>
         <tr>
           <th>WASP API</th>
           <td>
-            <CodeBlock> {props.evm.api} </CodeBlock>
+            <CodeBlock> {props.evm.custom.api} </CodeBlock>
           </td>
         </tr>
       </tbody>
@@ -140,17 +140,17 @@ function EvmCustom(props: NetworkProps) {
           <th>Chain Address</th>
           <td>
             <a
-              href={props.explorer + '/addr/' + props.evmCustom.chainAddress}
+              href={props.explorer + '/addr/' + props.evm.custom.chainAddress}
               target='_blank'
               rel='noopener noreferrer'
             >
-              {props.evmCustom.chainAddress}
+              {props.evm.custom.chainAddress}
             </a>
           </td>
         </tr>
         <tr>
           <th>Alias ID</th>
-          <td>{props.evmCustom.aliasId}</td>
+          <td>{props.evm.custom.aliasId}</td>
         </tr>
       </tbody>
     </table>
