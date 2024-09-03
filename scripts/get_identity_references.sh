bad9e6a210b7efcf63acc63bf1c5bd8f4156b0e4
Dr-Electron <dr-electr0n@protonmail.com>
2024-09-03 21:42:39 +0200

    Add new identity references

---
+++ scripts/get_identity_references.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ # Create temporaty directory to work in
+ mkdir tmp
+ cd tmp
+
+ # Download and copy docs
+ curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota-identity/1.3/wasm.tar.gz | tar xzv
+ cp -Rv docs/* ../docs/build/identity.rs/1.3/docs/references/
+
+ # Return to root and cleanup
+ cd -
+ rm -rf tmp
