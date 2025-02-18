1ce6b5e45a68aaad326860a017bd4b03b5fc72d0
Dr-Electron <dr-electr0n@protonmail.com>
2025-02-18 22:48:31 +0100

    Update Wasp docs to v1.6 (#1699)

--- scripts/get_wasp_references.sh
+++ scripts/get_wasp_references.sh
@@ -5,17 +5,17 @@ mkdir tmp
 cd tmp
 
 # Download and copy docs
-curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.3/iscmagic.tar.gz | tar xzv
-cp -Rv docs/iscmagic/* ../docs/build/isc/v1.3/docs/reference/magic-contract/
+curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.5/iscmagic.tar.gz | tar xzv
+cp -Rv docs/iscmagic/* ../docs/build/isc/v1.5/docs/reference/magic-contract/
 
-curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.3/iscutils.tar.gz | tar xzv
-cp -Rv docs/iscutils ../docs/build/isc/v1.3/docs/reference/
+curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.5/iscutils.tar.gz | tar xzv
+cp -Rv docs/iscutils ../docs/build/isc/v1.5/docs/reference/
 
-curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.4/iscmagic.tar.gz | tar xzv
-cp -Rv docs/iscmagic/* ../docs/build/isc/v1.4/docs/reference/magic-contract/
+curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.6/iscmagic.tar.gz | tar xzv
+cp -Rv docs/iscmagic/* ../docs/build/isc/v1.6/docs/reference/magic-contract/
 
-curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.4/iscutils.tar.gz | tar xzv
-cp -Rv docs/iscutils ../docs/build/isc/v1.4/docs/reference/
+curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/wasp/1.6/iscutils.tar.gz | tar xzv
+cp -Rv docs/iscutils ../docs/build/isc/v1.6/docs/reference/
 
 # Return to root and cleanup
 cd -
