38590a2fed928e730d7c1a88c22011086e9241f9
Dr-Electron <dr-electr0n@protonmail.com>
2025-02-18 21:53:28 +0100

    Remove unused components

--- src/theme/NavbarItem/ComponentTypes.tsx
+++
@@ -1,7 +0,0 @@
-import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
-import CustomNetworkDropdown from '@site/src/components/CustomNetworkDropdown';
-
-export default {
-  ...ComponentTypes,
-  'custom-network-dropdown': CustomNetworkDropdown,
-};
